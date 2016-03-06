cb1f61b6b655d700389d82930cf6b1c0386d9b74
Arpad Csanyi <csarpad@gmail.com>
2016-03-06 21:45:12 +0100

    Add new tasks for compiling sass files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
+var gulp = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    args = require('yargs').argv,
+    gulpif = require('gulp-if'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps');
+
+var paths = {
+  sassAll: 'sass/**/*.scss',
+  cssDest: 'css'
+}
 
 gulp.task('serve', function() {
   browserSync.init({
@@ -10,3 +19,17 @@ gulp.task('serve', function() {
 
   gulp.watch(['*.html', '*.css', '*.js']).on('change', browserSync.reload);
 });
+
+gulp.task('sass', function() {
+  return gulp.src(paths.sassAll)
+  .pipe(gulpif(args.debug, sourcemaps.init()))
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulpif(args.debug, sourcemaps.write()))
+  .pipe(gulp.dest(paths.cssDest));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.sassAll, ['sass']);
+});
+
+gulp.task('default', ['sass']);
