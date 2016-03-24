b6038d84d6eed3f3f56a28b328b3778f879b723c
László Monda <laci@monda.hu>
2016-03-24 22:32:48 +0100

    Add type definitions to the methods of UhkBuffer.

--- model/UhkBuffer.ts
+++ model/UhkBuffer.ts
@@ -2,42 +2,42 @@ class UhkBuffer {
     buffer: Buffer;
     offset: number;
 
-    constructor(buffer) {
+    constructor(buffer:Buffer) {
         this.offset = 0;
         this.buffer = buffer;
     }
 
-    writeInt8(value) {
+    writeInt8(value:number):void {
         this.buffer.writeInt8(value, this.offset);
         this.offset += 1;
     }
 
-    writeUint8(value) {
+    writeUint8(value:number):void {
         this.buffer.writeUInt8(value, this.offset);
         this.offset += 1;
     }
 
-    writeInt16(value) {
+    writeInt16(value:number):void {
         this.buffer.writeInt16LE(value, this.offset);
         this.offset += 2;
     }
 
-    writeUint16(value) {
+    writeUint16(value:number):void {
         this.buffer.writeUInt16LE(value, this.offset);
         this.offset += 2;
     }
 
-    writeInt32(value) {
+    writeInt32(value:number):void {
         this.buffer.writeInt32LE(value, this.offset);
         this.offset += 4;
     }
 
-    writeUint32(value) {
+    writeUint32(value:number):void {
         this.buffer.writeUInt32LE(value, this.offset);
         this.offset += 4;
     }
 
-    writeString(string) {
+    writeString(string:string):void {
         this.buffer.write(string, this.offset, string.length, 'ascii');
         this.offset += string.length;
         this.writeUint8(0);
