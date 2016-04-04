aac65cca2efaedfcbe9df574139fbad7152fb9de
László Monda <laci@monda.hu>
2016-04-04 02:34:44 +0200

    Dump every primitive type that UhkBuffer implements, not just uint8.

--- config-serializer/UhkBuffer.ts
+++ config-serializer/UhkBuffer.ts
@@ -20,21 +20,23 @@ class UhkBuffer {
 
     readInt8(): number {
         let value = this.buffer.readInt8(this.offset);
+        this.dump(`i8(${value})`);
         this.bytesToBacktrack = 1;
         this.offset += this.bytesToBacktrack;
         return value;
     }
 
     writeInt8(value: number): void {
+        this.dump(`i8(${value})`);
         this.buffer.writeInt8(value, this.offset);
         this.offset += 1;
     }
 
     readUInt8(): number {
         let value = this.buffer.readUInt8(this.offset);
+        this.dump(`u8(${value})`);
         this.bytesToBacktrack = 1;
         this.offset += this.bytesToBacktrack;
-        this.dump(`u8(${value})`);
         return value;
     }
 
@@ -46,48 +48,56 @@ class UhkBuffer {
 
     readInt16(): number {
         let value = this.buffer.readInt16LE(this.offset);
+        this.dump(`i16(${value})`);
         this.bytesToBacktrack = 2;
         this.offset += this.bytesToBacktrack;
         return value;
     }
 
     writeInt16(value: number): void {
+        this.dump(`i16(${value})`);
         this.buffer.writeInt16LE(value, this.offset);
         this.offset += 2;
     }
 
     readUInt16(): number {
         let value = this.buffer.readUInt16LE(this.offset);
+        this.dump(`u16(${value})`);
         this.bytesToBacktrack = 2;
         this.offset += this.bytesToBacktrack;
         return value;
     }
 
     writeUInt16(value: number): void {
+        this.dump(`u16(${value})`);
         this.buffer.writeUInt16LE(value, this.offset);
         this.offset += 2;
     }
 
     readInt32(): number {
         let value = this.buffer.readInt32LE(this.offset);
+        this.dump(`i32(${value})`);
         this.bytesToBacktrack = 4;
         this.offset += this.bytesToBacktrack;
         return value;
     }
 
     writeInt32(value: number): void {
+        this.dump(`i32(${value})`);
         this.buffer.writeInt32LE(value, this.offset);
         this.offset += 4;
     }
 
     readUInt32(): number {
         let value = this.buffer.readUInt32LE(this.offset);
+        this.dump(`u32(${value})`);
         this.bytesToBacktrack = 4;
         this.offset += this.bytesToBacktrack;
         return value;
     }
 
     writeUInt32(value: number): void {
+        this.dump(`u32(${value})`);
         this.buffer.writeUInt32LE(value, this.offset);
         this.offset += 4;
     }
@@ -97,10 +107,12 @@ class UhkBuffer {
         if (length === UhkBuffer.longCompactLengthPrefix) {
             length += this.readUInt8() << 8;
         }
+        this.dump(`cl(${length})`);
         return length;
     }
 
     writeCompactLength(length: number) {
+        this.dump(`cl(${length})`);
         if (length >= UhkBuffer.longCompactLengthPrefix) {
             this.writeUInt8(UhkBuffer.longCompactLengthPrefix);
             this.writeUInt16(length);
@@ -112,6 +124,7 @@ class UhkBuffer {
     readString(): string {
         let stringByteLength = this.readCompactLength();
         let str = this.buffer.toString(UhkBuffer.stringEncoding, this.offset, stringByteLength);
+        this.dump(`${UhkBuffer.stringEncoding}(${str})`);
         this.bytesToBacktrack = stringByteLength;
         this.offset += stringByteLength;
         return str;
@@ -126,6 +139,7 @@ class UhkBuffer {
         }
 
         this.writeCompactLength(stringByteLength);
+        this.dump(`${UhkBuffer.stringEncoding}(${str})`);
         this.buffer.write(str, this.offset, stringByteLength, UhkBuffer.stringEncoding);
         this.offset += stringByteLength;
     }
