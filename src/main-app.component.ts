dcf5cbe4d8e73785b713163d76b87d9b0e5d64b7
József Farkas <fjozsef001@gmail.com>
2016-04-06 21:44:53 +0200

    Add Angular2

---
+++ src/main-app.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from 'angular2/core';
+
+@Component({
+    selector: 'main-app',
+    template: 'It works!'
+})
+export class MainAppComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
