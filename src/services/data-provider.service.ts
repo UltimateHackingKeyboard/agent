df3ae6c91829161aca5c91e5e3fd3387b90b46cf
József Farkas <fjozsef001@gmail.com>
2016-04-09 21:29:22 +0200

    Load svg of the keyboard layout

---
+++ src/services/data-provider.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from 'angular2/core';
+
+@Injectable()
+export class DataProviderService {
+
+    constructor() { }
+
+
+    getBaseLayer(): any {
+        return require('xml!../../images/base-layer.svg').svg;
+    }
+
+}
