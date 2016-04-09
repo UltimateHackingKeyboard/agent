714f24429c24530c1465abf752c297dfd0e5aaef
József Farkas <fjozsef001@gmail.com>
2016-04-09 21:55:02 +0200

    Show keyboard at keymaps-ng2

--- src/main-app.component.ts
+++ src/main-app.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from 'angular2/core';
 
+import {KeyboardComponent} from './components/keyboard.component';
+
 @Component({
     selector: 'main-app',
-    template: 'It works!'
+    template: '<keyboard></keyboard>',
+    directives: [KeyboardComponent]
 })
 export class MainAppComponent implements OnInit {
     constructor() { }
