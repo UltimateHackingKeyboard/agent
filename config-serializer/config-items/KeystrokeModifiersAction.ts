9dde4e6bd71e9a47a51ed7f4ea82c495c4efe86d
László Monda <laci@monda.hu>
2016-04-09 13:35:19 +0200

    Add KeyModifiers enum.

--- config-serializer/config-items/KeystrokeModifiersAction.ts
+++ config-serializer/config-items/KeystrokeModifiersAction.ts
@@ -1,3 +1,14 @@
+enum KeyModifiers {
+    leftCtrl   = 1 << 0,
+    leftShift  = 1 << 1,
+    leftAlt    = 1 << 2,
+    leftGui    = 1 << 3,
+    rightCtrl  = 1 << 4,
+    rightShift = 1 << 5,
+    rightAlt   = 1 << 6,
+    rightGui   = 1 << 7
+}
+
 class KeystrokeModifiersAction extends KeyAction {
 
     @assertUInt8
