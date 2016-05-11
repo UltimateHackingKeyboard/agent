d4ef07a6b0b25deedf007b365d885cb00beca6fa
József Farkas <fjozsef001@gmail.com>
2016-05-11 20:50:00 +0200

    Add webpack as gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
-var gulp = require('gulp'),
-    browserSync = require('browser-sync').create(),
-    args = require('yargs').argv,
-    gulpif = require('gulp-if'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps');
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var args = require('yargs').argv;
+var gulpif = require('gulp-if');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var webpackStream = require('webpack-stream');
 
 var paths = {
   sassAll: 'sass/**/*.scss',
   cssDest: 'css'
 }
 
-gulp.task('serve', function() {
+gulp.task('serve', function () {
   browserSync.init({
     server: {
       baseDir: "./"
@@ -20,16 +21,22 @@ gulp.task('serve', function() {
   gulp.watch(['*.html', '*.css', '*.js']).on('change', browserSync.reload);
 });
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   return gulp.src(paths.sassAll)
-  .pipe(gulpif(args.debug, sourcemaps.init()))
-  .pipe(sass().on('error', sass.logError))
-  .pipe(gulpif(args.debug, sourcemaps.write()))
-  .pipe(gulp.dest(paths.cssDest));
+    .pipe(gulpif(args.debug, sourcemaps.init()))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulpif(args.debug, sourcemaps.write()))
+    .pipe(gulp.dest(paths.cssDest));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch(paths.sassAll, ['sass']);
 });
 
-gulp.task('default', ['sass']);
+gulp.task('webpack', function () {
+  return gulp.src('./src/boot.ts')
+    .pipe(webpackStream(require('./webpack.config.js')))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('default', ['sass', 'webpack']);
