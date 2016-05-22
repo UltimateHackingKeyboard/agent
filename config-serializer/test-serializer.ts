a3396334ae963f734014528040373780e4ee5a74
József Farkas <fjozsef001@gmail.com>
2016-05-22 23:50:54 +0200

    Update typings to 1.0.x.

--- config-serializer/test-serializer.ts
+++ config-serializer/test-serializer.ts
@@ -1,4 +1,4 @@
-/// <reference path="../typings/main/ambient/node/index.d.ts" />
+/// <reference path="../typings/globals/node/index.d.ts" />
 
 import {Serializable} from './Serializable';
 import {UhkBuffer} from './UhkBuffer';
