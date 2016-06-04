4ea1d766b4ca24a069ce117d67179054d1742dab
József Farkas <fjozsef001@gmail.com>
2016-06-04 17:19:00 +0200

    Add icon component

---
+++ src/components/popover/widgets/icon.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { NgSwitch, NgSwitchWhen } from '@angular/common';
+
+@Component({
+    moduleId: module.id,
+    selector: 'icon',
+    template:
+    `
+        <div [ngSwitch]="name">
+            <span *ngSwitchWhen="'option-vertical'" class="glyphicon glyphicon-option-vertical" aria-hidden="true"></span>
+            <i *ngSwitchWhen="'square'" class="fa fa-square"></i>
+            <i *ngSwitchWhen="'mouse-pointer'" class="fa fa-mouse-pointer"></i>
+            <i *ngSwitchWhen="'clock'" class="fa fa-clock-o"></i>
+            <i *ngSwitchWhen="'trash'" class="glyphicon glyphicon-trash action--trash"></i>
+            <i *ngSwitchWhen="'pencil'" class="glyphicon glyphicon-pencil action--edit"></i>
+        </div>
+    `,
+    directives: [NgSwitch, NgSwitchWhen],
+    styles: [require('./icon.component.scss')]
+})
+export class IconComponent implements OnInit {
+
+    @Input() name: string;
+
+    constructor() { }
+
+    ngOnInit() { }
+
+}
