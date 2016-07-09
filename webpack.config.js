d367a85860f6e1aafcce89c9163f3601130be3d6
NejcZdovc <nejc.zdovc@3zsistemi.si>
2016-07-09 14:14:27 +0200

    New webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,25 @@
-// var webpack = require("webpack");
+var webpack = require('webpack');
+var path = require('path');
 var SvgStore = require('webpack-svgstore-plugin');
 
-module.exports = {
-    entry: ['reflect-metadata', 'zone.js', 'es6-shim', "./src/boot.ts"],
-    output: {
-        path: __dirname + "/build",
-        filename: "uhk.js"
-    },
-    devtool: 'source-map',
-    resolve: {
-        extensions: ['', '.webpack.js', '.web.js', '.ts', '.js'],
-        alias: {
 
-        },
-        modulesDirectories: [
-            'node_modules',
-            'bower_components'
-        ]
+// Webpack Config
+var webpackConfig = {
+    entry: {
+        'polyfills': './src/polyfills.browser.ts',
+        'main':       './src/main.browser.ts',
     },
-    module: {
-        loaders: [
-            { test: /\.ts$/, loader: 'ts-loader', exclude: /node_modules/ },
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                loaders: ['raw-loader', 'sass-loader']
-            }
-        ]
+
+    output: {
+        path: './build',
+        filename: '[name].bundle.js',
+        sourceMapFilename: '[name].map',
+        chunkFilename: '[id].chunk.js'
     },
+
     plugins: [
-        //   new webpack.optimize.UglifyJsPlugin({ minimize: true })
+        new webpack.optimize.OccurenceOrderPlugin(true),
+        new webpack.optimize.CommonsChunkPlugin({ name: ['main', 'polyfills'], minChunks: Infinity }),
         new SvgStore(
             [
                 'images/icons/**/*.svg'
@@ -45,6 +35,58 @@ module.exports = {
                 }
             }
         )
-    ]
+    ],
 
-}
+    module: {
+        loaders: [
+            // .ts files for TypeScript
+            { test: /\.ts$/, loaders: ['awesome-typescript-loader', 'angular2-template-loader'] },
+            { test: /\.html$/, loader: 'raw-loader' },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                loaders: ['raw-loader', 'sass-loader']
+            }
+        ],
+        preLoaders: [
+            {
+                test: /\.js$/,
+                loader: 'source-map-loader',
+                exclude: [
+                    // these packages have problems with their sourcemaps
+                    path.join(__dirname, 'node_modules', 'rxjs'),
+                    path.join(__dirname, 'node_modules', '@angular2-material'),
+                    path.join(__dirname, 'node_modules', '@angular'),
+                ]
+            }
+        ],
+        noParse: [
+            path.join(__dirname, 'node_modules', 'zone.js', 'dist'),
+            path.join(__dirname, 'node_modules', 'angular2', 'bundles')
+        ]
+    },
+    devtool: 'cheap-module-source-map',
+    cache: true,
+    debug: true,
+
+    resolve: {
+        root: [ path.join(__dirname, 'src') ],
+        extensions: ['', '.ts', '.js', '.json']
+    },
+
+    devServer: {
+        historyApiFallback: true,
+        watchOptions: { aggregateTimeout: 300, poll: 1000 },
+        port: '3003',
+        contentBase: './'
+    },
+
+    node: {
+        global: 1,
+        module: 0,
+        clearImmediate: 0,
+        setImmediate: 0
+    }
+};
+
+module.exports = webpackConfig;
