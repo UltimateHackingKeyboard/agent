847382b3c432dbc71c7bed731977eae6b1c86756
NejcZdovc <nejc.zdovc@3zsistemi.si>
2016-08-15 12:08:16 +0200

    Added notification component

---
+++ src/components/notification/notification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'notification',
+    template: require('./notification.component.html'),
+    styles: [require('./notification.component.scss')]
+})
+export class NotificationComponent {
+
+    constructor() {
+    }
+}
--- src/main-app.component.ts
+++ src/main-app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 
 import { SideMenuComponent } from './components/sidemenu/side-menu.component';
+import { NotificationComponent } from './components/notification/notification.component';
 
 @Component({
     moduleId: module.id,
     selector: 'main-app',
     template: require('./main-app.component.html'),
     styles: [require('./main-app.component.scss')],
-    directives: [SideMenuComponent, ROUTER_DIRECTIVES]
+    directives: [SideMenuComponent, ROUTER_DIRECTIVES, NotificationComponent]
 })
 export class MainAppComponent  {
     constructor() {
