25a53df891b9bc4f4e4380b49ef47a519024b2e3
József Farkas <fjozsef001@gmail.com>
2016-08-28 09:01:52 +0200

    refactor: Layers (#85)

--- src/components/layers/layers.component.ts
+++ src/components/layers/layers.component.ts
@@ -1,6 +1,4 @@
-import {
-    Component, Output, EventEmitter, ElementRef, QueryList, ViewChildren, Renderer, Input
-} from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'layers',
@@ -9,43 +7,25 @@ import {
 })
 export class LayersComponent {
     @Input() current: number;
-    @Output() selected = new EventEmitter();
+    @Output() select = new EventEmitter();
 
-    @ViewChildren('baseButton,modButton,fnButton,mouseButton')
-    buttonsQueryList: QueryList<ElementRef>;
+    private buttons: string[];
 
-    private buttons: ElementRef[];
-    private selectedLayerIndex: number;
-
-    constructor(private renderer: Renderer) {
-        this.buttons = [];
-        this.selectedLayerIndex = 0;
-    }
-
-    ngOnChanges() {
-        if (this.buttons.length > 0 && this.current !== this.selectedLayerIndex) {
-            this.buttons.forEach((button: ElementRef) => {
-                this.renderer.setElementClass(button.nativeElement, 'btn-primary', false);
-            });
-            this.renderer.setElementClass(this.buttons[this.current].nativeElement, 'btn-primary', true);
-            this.selectedLayerIndex = 0;
-        }
+    constructor() {
+        this.buttons = ['Base', 'Mod', 'Fn', 'Mouse'];
+        this.current = 0;
     }
 
     selectLayer(index: number) {
-        if (index === this.selectedLayerIndex) {
+        if (this.current === index) {
             return;
         }
 
-        this.buttons = this.buttonsQueryList.toArray();
-        this.selected.emit({
-            oldIndex: this.selectedLayerIndex,
+        this.select.emit({
+            oldIndex: this.current,
             index: index
         });
 
-        this.renderer.setElementClass(this.buttons[this.selectedLayerIndex].nativeElement, 'btn-primary', false);
-        this.renderer.setElementClass(this.buttons[index].nativeElement, 'btn-primary', true);
-
-        this.selectedLayerIndex = index;
+        this.current = index;
     }
 }
