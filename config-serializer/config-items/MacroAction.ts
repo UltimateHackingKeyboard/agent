fa284040f1d29ec9a9dad67e950dcc36eaafc23d
Mikko Lakomaa <hopealusikka@gmail.com>
2016-08-28 14:32:48 +0300

    Added: clone method to MacroAction

--- config-serializer/config-items/MacroAction.ts
+++ config-serializer/config-items/MacroAction.ts
@@ -51,8 +51,26 @@ export abstract class MacroAction extends Serializable<MacroAction> {
         }
     }
 
+    /**
+     * Clone MacroAction for editing without changing original
+     * @param {any} [obj] Object attribute to clone, should be left empty
+     * @return {any}
+     */
+    clone(obj: any = undefined): any {
+        let cloneObj = new (<any>this.constructor)();
+        for (let attribute in this) {
+            if (typeof this[attribute] === "object") {
+                cloneObj[attribute] = this.clone(this[attribute]);
+            } else {
+                cloneObj[attribute] = this[attribute];
+            }
+        }
+        return cloneObj;
+    }
+
     abstract _fromJsObject(jsObject: any): MacroAction;
     abstract _fromBinary(buffer: UhkBuffer): MacroAction;
     abstract _toJsObject(): any;
     abstract _toBinary(buffer: UhkBuffer): void;
+
 }
