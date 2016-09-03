c555f46d5498d4594e1b4b031af84023f3c035d3
Mikko Lakomaa <hopealusikka@gmail.com>
2016-09-03 18:38:30 +0300

    Changed: Render selected keys and mouse buttons in MacroItemComponent, refactor code for rendering macro item titles to be more readable

--- src/components/macro/macro-item/macro-item.component.ts
+++ src/components/macro/macro-item/macro-item.component.ts
@@ -1,28 +1,30 @@
 import { Component, OnInit, OnChanges, Input, Output, ViewChild, EventEmitter } from '@angular/core';
 
-import {MacroAction} from '../../../config-serializer/config-items/MacroAction';
+import {MacroAction, macroActionType} from '../../../config-serializer/config-items/MacroAction';
 
 import {DelayMacroAction} from '../../../config-serializer/config-items/DelayMacroAction';
 import {TextMacroAction} from '../../../config-serializer/config-items/TextMacroAction';
 
-import {PressMouseButtonsMacroAction} from '../../../config-serializer/config-items/PressMouseButtonsMacroAction';
-import {HoldMouseButtonsMacroAction} from '../../../config-serializer/config-items/HoldMouseButtonsMacroAction';
-import {ReleaseMouseButtonsMacroAction} from '../../../config-serializer/config-items/ReleaseMouseButtonsMacroAction';
-
+import {MouseButtonMacroAction} from '../../../config-serializer/config-items/MouseButtonMacroAction';
 import {MoveMouseMacroAction} from '../../../config-serializer/config-items/MoveMouseMacroAction';
 import {ScrollMouseMacroAction} from '../../../config-serializer/config-items/ScrollMouseMacroAction';
 
-import {PressKeyMacroAction} from '../../../config-serializer/config-items/PressKeyMacroAction';
-import {HoldKeyMacroAction} from '../../../config-serializer/config-items/HoldKeyMacroAction';
-import {ReleaseKeyMacroAction} from '../../../config-serializer/config-items/ReleaseKeyMacroAction';
-
-import {PressModifiersMacroAction} from '../../../config-serializer/config-items/PressModifiersMacroAction';
-import {HoldModifiersMacroAction} from '../../../config-serializer/config-items/HoldModifiersMacroAction';
-import {ReleaseModifiersMacroAction} from '../../../config-serializer/config-items/ReleaseModifiersMacroAction';
+import {KeyMacroAction} from '../../../config-serializer/config-items/KeyMacroAction';
 
 import {KeyModifiers}  from '../../../config-serializer/config-items/KeyModifiers';
 
 import { MacroActionEditorComponent } from '../macro-action-editor/macro-action-editor.component';
+
+// Flatten scancodes for easier label retrieval
+// @todo Should this be its own importable file?
+const scancodesJSON = require('json!../../popover/tab/keypress/scancodes.json');
+let scancodes: any[] = [];
+for (let i = 0, len = scancodesJSON.length; i < len; i++) {
+    const group = scancodesJSON[i];
+    if (group.children.length) {
+        scancodes = scancodes.concat(group.children);
+    }
+}
 
 @Component({
     selector: 'macro-item',
@@ -50,8 +52,6 @@ export class MacroItemComponent implements OnInit, OnChanges {
     /* tslint:disable:no-unused-variable: It is used in the template. */
     private customTitle: string;
     /* tslint:enable:no-unused-variable */
-
-    constructor() { }
 
     ngOnInit() {
         this.updateView();
@@ -90,52 +90,7 @@ export class MacroItemComponent implements OnInit, OnChanges {
     private updateView(): void {
         this.title = this.macroAction.constructor.name;
 
-        if (this.macroAction instanceof MoveMouseMacroAction) {
-            // Mouse moved
-            this.iconName = 'mouse-pointer';
-            this.title = 'Move pointer';
-
-            let action: MoveMouseMacroAction = this.macroAction as MoveMouseMacroAction;
-            let needAnd: boolean;
-            if (Math.abs(action.x) !== 0) {
-                this.title += ` by ${Math.abs(action.x)}px ${action.x > 0 ? 'left' : 'right'}`;
-                needAnd = true;
-            }
-            if (Math.abs(action.y) !== 0) {
-                this.title += ` ${needAnd ? 'and' : 'by'} ${Math.abs(action.y)}px ${action.y > 0 ? 'down' : 'up'}`;
-            }
-        } else if (this.macroAction instanceof ScrollMouseMacroAction) {
-            // Scroll mouse
-            this.iconName = 'mouse-pointer';
-            this.title = 'Scroll';
-            let action: ScrollMouseMacroAction = this.macroAction as ScrollMouseMacroAction;
-            let needAnd: boolean;
-            if (Math.abs(action.x) !== 0) {
-                this.title += ` by ${Math.abs(action.x)}px ${action.x > 0 ? 'left' : 'right'}`;
-                needAnd = true;
-            }
-            if (Math.abs(action.y) !== 0) {
-                this.title += ` ${needAnd ? 'and' : 'by'} ${Math.abs(action.y)}px ${action.y > 0 ? 'down' : 'up'}`;
-            }
-        } else if (this.macroAction instanceof PressMouseButtonsMacroAction) {
-            // Press mouse buttons
-            let action: PressMouseButtonsMacroAction = this.macroAction as PressMouseButtonsMacroAction;
-            this.iconName = 'mouse-pointer';
-            this.title = 'Click mouse button: <some mouse button>';
-            // @todo COMPLETE THIS
-        } else if (this.macroAction instanceof HoldMouseButtonsMacroAction) {
-            // Hold mouse buttons
-            let action: HoldMouseButtonsMacroAction = this.macroAction as HoldMouseButtonsMacroAction;
-            this.iconName = 'hand-rock';
-            this.title = 'Hold mouse button: <some mouse button>';
-            // @todo COMPLETE THIS
-        } else if (this.macroAction instanceof ReleaseMouseButtonsMacroAction) {
-            // Release mouse buttons
-            let action: ReleaseMouseButtonsMacroAction = this.macroAction as ReleaseMouseButtonsMacroAction;
-            this.iconName = 'hand-paper';
-            this.title = 'Release mouse button: <some mouse button>';
-            // @todo COMPLETE THIS
-        } else if (this.macroAction instanceof DelayMacroAction) {
+        if (this.macroAction instanceof DelayMacroAction) {
             // Delay
             this.iconName = 'clock';
             let action: DelayMacroAction = this.macroAction as DelayMacroAction;
@@ -146,68 +101,116 @@ export class MacroItemComponent implements OnInit, OnChanges {
             let action: TextMacroAction = this.macroAction as TextMacroAction;
             this.iconName = 'font';
             this.title = `Write text: ${action.text}`;
-        } else if (this.macroAction instanceof PressKeyMacroAction) {
-            // Press key
-            let action: PressKeyMacroAction = this.macroAction as PressKeyMacroAction;
-            this.iconName = 'hand-pointer';
-            this.title = 'Press key: ' + action.scancode;
-            // @todo COMPLETE THIS
-        } else if (this.macroAction instanceof HoldKeyMacroAction) {
-            // Press key
-            let action: HoldKeyMacroAction = this.macroAction as HoldKeyMacroAction;
-            this.iconName = 'hand-rock';
-            this.title = 'Hold key: ' + action.scancode;
-            // @todo COMPLETE THIS
-        } else if (this.macroAction instanceof ReleaseKeyMacroAction) {
-            // Release key
-            let action: ReleaseKeyMacroAction = this.macroAction as ReleaseKeyMacroAction;
-            this.iconName = 'hand-paper';
-            this.title = 'Release key: ' + action.scancode;
-            // @todo COMPLETE THIS
-        } else if (this.macroAction instanceof PressModifiersMacroAction) {
-            // Press modifiers
-            this.iconName = 'hand-pointer';
-            let action: PressModifiersMacroAction = this.macroAction as PressModifiersMacroAction;
-            if (action.modifierMask === 0) {
-                this.title = 'Invalid PressModifiersMacroAction!';
+        } else if (this.macroAction instanceof KeyMacroAction) {
+            const action: KeyMacroAction = this.macroAction as KeyMacroAction;
+            this.setKeyActionContent(action);
+        } else if (this.macroAction instanceof MouseButtonMacroAction) {
+            const action: MouseButtonMacroAction = this.macroAction as MouseButtonMacroAction;
+            this.setMouseButtonActionContent(action);
+        } else if (this.macroAction instanceof MoveMouseMacroAction || this.macroAction instanceof ScrollMouseMacroAction) {
+            // Mouse moved or scrolled
+           this.setMouseMoveScrollActionContent(this.macroAction);
+        }
+        // TODO: finish for all MacroAction
+    }
+
+    private setKeyActionContent(action: KeyMacroAction) {
+        if (action.isKeyAction())  {
+            // Press/hold/release key
+            if (action.scancode === 0) {
+                this.title = 'Invalid keypress';
                 return;
+            } else if (action.macroActionType === macroActionType.PressKeyMacroAction) {
+                this.iconName = 'hand-pointer';
+                this.title = 'Press key: ';
+            } else if (action.macroActionType === macroActionType.HoldKeyMacroAction) {
+                // Press key
+                this.iconName = 'hand-rock';
+                this.title = 'Hold key: ';
+            } else if (action.macroActionType === macroActionType.ReleaseKeyMacroAction) {
+                // Release key
+                this.iconName = 'hand-paper';
+                this.title = 'Release key: ';
             }
-            this.title = 'Press modifier: ';
-            for (let i = KeyModifiers.leftCtrl; i !== KeyModifiers.rightCtrl; i <<= 1) {
-                if (action.isModifierActive(i)) {
-                    this.title += ' ' + KeyModifiers[i];
-                }
+
+            this.title += this.getScancodeLabel(action.scancode);
+        }
+
+        if (action.isModifierAction() && action.modifierMask) {
+            // Press/hold/release modifiers
+            if (action.macroActionType === macroActionType.PressModifiersMacroAction) {
+                this.iconName = 'hand-pointer';
+                this.title = 'Press modifier: ';
+            } else if (action.macroActionType === macroActionType.HoldModifiersMacroAction) {
+                this.iconName = 'hand-rock';
+                this.title = 'Hold modifier: ';
+            } else if (action.macroActionType === macroActionType.ReleaseModifiersMacroAction) {
+                this.iconName = 'hand-paper';
+                this.title = 'Release modifier: ';
             }
-        } else if (this.macroAction instanceof HoldModifiersMacroAction) {
-            // Hold modifiers
-            this.iconName = 'hand-rock';
-            let action: HoldModifiersMacroAction = this.macroAction as HoldModifiersMacroAction;
-            if (action.modifierMask === 0) {
-                this.title = 'Invalid HoldModifiersMacroAction!';
-                return;
-            }
-            this.title = 'Hold modifier: ';
-            for (let i = KeyModifiers.leftCtrl; i !== KeyModifiers.rightCtrl; i <<= 1) {
-                if (action.isModifierActive(i)) {
-                    this.title += ' ' + KeyModifiers[i];
-                }
-            }
-        } else if (this.macroAction instanceof ReleaseModifiersMacroAction) {
-            // Release modifiers
-            this.iconName = 'hand-paper';
-            let action: ReleaseModifiersMacroAction = this.macroAction as ReleaseModifiersMacroAction;
-            if (action.modifierMask === 0) {
-                this.title = 'Invalid ReleaseModifiersMacroAction!';
-                return;
-            }
-            this.title = 'Release modifier: ';
-            for (let i = KeyModifiers.leftCtrl; i !== KeyModifiers.rightCtrl; i <<= 1) {
+
+            for (let i = KeyModifiers.leftCtrl; i !== KeyModifiers.rightGui; i <<= 1) {
                 if (action.isModifierActive(i)) {
                     this.title += ' ' + KeyModifiers[i];
                 }
             }
         }
-        // TODO: finish for all MacroAction
     }
 
+    private getScancodeLabel(scancode: number): string {
+        const scancodeStr: string = scancode.toString();
+        for (let i = 0, len = scancodes.length; i < len; i++) {
+            if (scancodes[i].id === scancodeStr) {
+                return scancodes[i].text;
+            }
+        }
+        return '';
+    }
+
+    private setMouseMoveScrollActionContent(action: MacroAction) {
+        let typedAction: any;
+        if (action.macroActionType === macroActionType.MoveMouseMacroAction) {
+            this.iconName = 'mouse-pointer';
+            this.title = 'Move pointer';
+            typedAction = this.macroAction as MoveMouseMacroAction;
+        } else {
+            // Scroll mouse
+            this.iconName = 'mouse-pointer';
+            this.title = 'Scroll';
+            typedAction = this.macroAction as ScrollMouseMacroAction;
+        }
+
+        let needAnd: boolean;
+        if (Math.abs(typedAction.x) !== 0) {
+            this.title += ` by ${Math.abs(typedAction.x)}px ${typedAction.x > 0 ? 'left' : 'right'}`;
+            needAnd = true;
+        }
+        if (Math.abs(typedAction.y) !== 0) {
+            this.title += ` ${needAnd ? 'and' : 'by'} ${Math.abs(typedAction.y)}px ${typedAction.y > 0 ? 'down' : 'up'}`;
+        }
+    }
+
+    private setMouseButtonActionContent(action: MouseButtonMacroAction) {
+        // Press/hold/release mouse buttons
+        if (action.macroActionType === macroActionType.PressMouseButtonsMacroAction) {
+            this.iconName = 'mouse-pointer';
+            this.title = 'Click mouse button: ';
+        } else if (action.macroActionType === macroActionType.HoldMouseButtonsMacroAction) {
+            this.iconName = 'hand-rock';
+            this.title = 'Hold mouse button: ';
+        } else if (action.macroActionType === macroActionType.ReleaseMouseButtonsMacroAction) {
+            this.iconName = 'hand-paper';
+            this.title = 'Release mouse button: ';
+        }
+
+        const buttonLabels: string[] = ['Left', 'Middle', 'Right'];
+        const selectedButtons: boolean[] = action.bitMaskToBooleans();
+        const selectedButtonLabels: string[] = [];
+        selectedButtons.forEach((isSelected, idx) => {
+            if (isSelected && buttonLabels[idx]) {
+                selectedButtonLabels.push(buttonLabels[idx]);
+            }
+        });
+        this.title += selectedButtonLabels.join(', ');
+    }
 }
