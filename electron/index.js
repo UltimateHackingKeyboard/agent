858fa0d87ceb96ae687a5a37beaa681fc6e7f1c1
Farkas József <fjozsef001@gmail.com>
2016-09-04 20:15:25 +0200

    Experimental electron support fixes

--- electron/index.js
+++ electron/index.js
@@ -17,10 +17,7 @@ function createWindow() {
 
     const indexPath = path.resolve(__dirname, '../dist/index.html');
     // and load the index.html of the app.
-    win.loadURL(`file://${indexPath}`)
-
-    // Open the DevTools.
-    win.webContents.openDevTools()
+    win.loadURL(`file://${indexPath}`);
 
     // Emitted when the window is closed.
     win.on('closed', () => {
@@ -28,13 +25,13 @@ function createWindow() {
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
         win = null;
-    })
+    });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -43,7 +40,7 @@ app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
-})
+});
 
 app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
@@ -51,7 +48,7 @@ app.on('activate', () => {
     if (win === null) {
         createWindow()
     }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here
