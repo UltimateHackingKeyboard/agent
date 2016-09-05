87d479fcf87ee011d6799398191082db1e4b99b2
Mikko Lakomaa <hopealusikka@gmail.com>
2016-09-05 20:33:29 +0300

    Fixed: app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { Select2Component } from 'ng2-select2/ng2-select2';
 
@@ -8,7 +9,6 @@ import { MainAppComponent, appRoutingProviders, routing }  from './main-app';
 import { KeymapAddComponent, KeymapComponent } from './components/keymap';
 import { LayersComponent } from './components/layers';
 import { LegacyLoaderComponent } from './components/legacy-loader';
-import { MacroComponent } from './components/macro';
 import { NotificationComponent } from './components/notification';
 import { PopoverComponent } from './components/popover';
 import {
@@ -19,7 +19,6 @@ import {
     MouseTabComponent,
     NoneTabComponent
 } from './components/popover/tab';
-import { MacroItemComponent } from './components/popover/tab/macro';
 import { CaptureKeystrokeButtonComponent } from './components/popover/widgets/capture-keystroke';
 import { IconComponent } from './components/popover/widgets/icon';
 import { SideMenuComponent } from './components/side-menu';
@@ -37,16 +36,28 @@ import {
 import { SvgModuleComponent } from './components/svg/module';
 import { SvgKeyboardWrapComponent } from './components/svg/wrap';
 
+import {
+    MacroComponent,
+    MacroItemComponent,
+    MacroActionEditorComponent,
+    MacroDelayTabComponent,
+    MacroKeyTabComponent,
+    MacroMouseTabComponent,
+    MacroTextTabComponent
+} from './components/macro';
+
+import { ContenteditableModel } from './components/contenteditable';
+import { Dragula } from 'ng2-dragula/ng2-dragula';
+
 import { DataProviderService } from './services/data-provider.service';
 import { MapperService } from './services/mapper.service';
-import {UhkConfigurationService} from './services/uhk-configuration.service';
+import { UhkConfigurationService } from './services/uhk-configuration.service';
 
 @NgModule({
     declarations: [
         Select2Component,
         MainAppComponent,
         KeymapComponent,
-        MacroComponent,
         LegacyLoaderComponent,
         NotificationComponent,
         SvgIconTextKeyComponent,
@@ -73,10 +84,19 @@ import {UhkConfigurationService} from './services/uhk-configuration.service';
         NoneTabComponent,
         CaptureKeystrokeButtonComponent,
         IconComponent,
-        MacroItemComponent
+        MacroComponent,
+        MacroItemComponent,
+        MacroActionEditorComponent,
+        MacroDelayTabComponent,
+        MacroKeyTabComponent,
+        MacroMouseTabComponent,
+        MacroTextTabComponent,
+        ContenteditableModel,
+        Dragula
     ],
     imports: [
         BrowserModule,
+        FormsModule,
         routing
     ],
     providers: [
