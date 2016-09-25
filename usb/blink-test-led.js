554a7b45f63155051f21b982c0267ba7175a5220
László Monda <laci@monda.hu>
2016-09-26 01:56:46 +0200

    Send 0 and 1 instead of their ASCII counterparts.

--- usb/blink-test-led.js
+++ usb/blink-test-led.js
@@ -18,22 +18,16 @@ usbInterface.claim();
 var endpointIn = usbInterface.endpoints[0];
 var endpointOut = usbInterface.endpoints[1];
 
-var state = '1';
+var state = 1;
 
 setInterval(function() {
     console.log('Sending ', state);
-    state = state === '1' ? '0' : '1'
-    endpointOut.transfer(state, function(err) {
+    state = state ? 0 : 1
+    console.log(state)
+    endpointOut.transfer(new Buffer([state]), function(err) {
         if (err) {
             console.error("USB error: %s", err);
             process.exit(1);
         }
-        endpointIn.transfer(64, function(err2, receivedStr) {
-            if (err2) {
-                console.error("USB error: %s", err2);
-                process.exit(2);
-            }
-            console.log('Received "%s"', receivedStr);
-        })
     });
 }, 500)
