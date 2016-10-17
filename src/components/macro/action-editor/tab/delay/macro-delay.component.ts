51c33cec3cd168d19ef4e554f7c38dc46dc6eaa6
NejcZdovc <nejc.zdovc@3zsistemi.si>
2016-10-17 14:35:16 +0200

    Fixed delay tab error

--- src/components/macro/action-editor/tab/delay/macro-delay.component.ts
+++ src/components/macro/action-editor/tab/delay/macro-delay.component.ts
@@ -1,4 +1,11 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, Renderer, ViewChild } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    ElementRef,
+    Input,
+    OnInit,
+    ViewChild
+} from '@angular/core';
 
 import { EditableMacroAction } from '../../../../../config-serializer/config-items/macro-action';
 
@@ -8,24 +15,22 @@ const INITIAL_DELAY = 0.5; // In seconds
     selector: 'macro-delay-tab',
     template: require('./macro-delay.component.html'),
     styles: [require('./macro-delay.component.scss')],
-    host: { 'class': 'macro__delay' }
+    host: { 'class': 'macro__delay' },
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MacroDelayTabComponent implements AfterViewInit, OnInit {
+export class MacroDelayTabComponent implements OnInit {
     @Input() macroAction: EditableMacroAction;
     @ViewChild('macroDelayInput') input: ElementRef;
+
     private delay: number;
     /* tslint:disable:no-unused-variable: It is used in the template. */
     private presets: number[] = [0.3, 0.5, 0.8, 1, 2, 3, 4, 5];
     /* tslint:enable:no-unused-variable */
 
-    constructor(private renderer: Renderer) { }
+    constructor() { }
 
     ngOnInit() {
         this.delay = this.macroAction.delay > 0 ? this.macroAction.delay / 1000 : INITIAL_DELAY;
-    }
-
-    ngAfterViewInit() {
-        this.renderer.invokeElementMethod(this.input.nativeElement, 'focus');
     }
 
     setDelay(value: number): void {
