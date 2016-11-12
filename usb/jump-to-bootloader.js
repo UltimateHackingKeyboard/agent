e294b259e2fcdc452cd3fe08b0c0b98c1f2117b7
László Monda <laci@monda.hu>
2016-11-12 22:03:17 +0100

    Add jump to bootloader script.

---
+++ usb/jump-to-bootloader.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+'use strict';
+
+var usb = require('usb');
+var util = require('./util');
+
+var vid = 0x16d3;
+var pid = 0x05ea;
+var jumpToBootloaderCommandId = 1;
+
+var device = usb.findByIds(vid, pid);
+device.open();
+
+var usbInterface = device.interface(0);
+
+// https://github.com/tessel/node-usb/issues/147
+// The function 'isKernelDriverActive' is not available on Windows and not even needed.
+if (process.platform !== 'win32' && usbInterface.isKernelDriverActive()) {
+    usbInterface.detachKernelDriver();
+}
+usbInterface.claim();
+
+var endpointIn = usbInterface.endpoints[0];
+var endpointOut = usbInterface.endpoints[1];
+
+var payload = new Buffer([jumpToBootloaderCommandId]);
+console.log('Sending ', util.bufferToString(payload));
+endpointOut.transfer(payload, function(err) {
+    if (err) {
+        console.error("USB error: %s", err);
+        process.exit(1);
+    }
+    endpointIn.transfer(64, function(err2, receivedBuffer) {
+        if (err2) {
+            console.error("USB error: %s", err2);
+            process.exit(2);
+        }
+        console.log('Received', util.bufferToString(receivedBuffer));
+    })
+});
