ae7d90e634b96dd3ac9700c5b6cadf88a1fdd61e
Nejc Zdovc <NejcZdovc@users.noreply.github.com>
2017-01-02 22:21:47 +0100

    Added menu key (#245)
    Resolves #243

--- src/components/popover/widgets/capture-keystroke/capture-keystroke-button.component.ts
+++ src/components/popover/widgets/capture-keystroke/capture-keystroke-button.component.ts
@@ -11,16 +11,22 @@ export class CaptureKeystrokeButtonComponent {
 
     private record: boolean;
     private first: boolean; // enable usage of Enter to start capturing
+    private scanCodePressed: boolean;
 
     constructor(private captureService: CaptureService) {
         this.record = false;
         this.captureService.initModifiers();
         this.captureService.populateMapping();
+        this.scanCodePressed = false;
     }
 
-    @HostListener('keyup')
-    onKeyUp() {
-        if (this.record && !this.first) {
+    @HostListener('keyup', ['$event'])
+    onKeyUp(e: KeyboardEvent) {
+        if (this.scanCodePressed) {
+            e.preventDefault();
+            this.scanCodePressed = false;
+        } else if (this.record && !this.first) {
+            e.preventDefault();
             this.saveScanCode();
         }
     }
@@ -37,6 +43,7 @@ export class CaptureKeystrokeButtonComponent {
 
             if (this.captureService.hasMap(code)) {
                 this.saveScanCode(this.captureService.getMap(code));
+                this.scanCodePressed = true;
             } else {
                 this.captureService.setModifier((e.location === 1), code);
             }
