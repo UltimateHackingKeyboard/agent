e54c1df13e2b580d6cf9771c277f094aa50423a8
László Monda <laci@monda.hu>
2017-01-15 03:24:33 +0100

    Vastly simplify jump-to-bootloader.js by using uhk.sendUsbPacket()

--- usb/jump-to-bootloader.js
+++ usb/jump-to-bootloader.js
@@ -1,19 +1,3 @@
 #!/usr/bin/env node
 let uhk = require('./uhk');
-let [endpointIn, endpointOut] = uhk.getUsbEndpoints();
-
-var payload = new Buffer([uhk.usbCommands.jumpToBootloader]);
-console.log('Sending ', uhk.bufferToString(payload));
-endpointOut.transfer(payload, function(err) {
-    if (err) {
-        console.error("USB error: %s", err);
-        process.exit(1);
-    }
-    endpointIn.transfer(64, function(err2, receivedBuffer) {
-        if (err2) {
-            console.error("USB error: %s", err2);
-            process.exit(2);
-        }
-        console.log('Received', uhk.bufferToString(receivedBuffer));
-    })
-});
+uhk.sendUsbPacket(new Buffer([uhk.usbCommands.jumpToBootloader]));
