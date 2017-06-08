b224f63544df8c6040dc4ae73163dcb28a0bfb55
Róbert Kiss <ert78gb@gmail.com>
2017-06-08 16:23:10 +0200

    style(privilege): Fix typo 'excusive' -> 'exclusive'

--- electron/src/services/uhk-device.service.ts
+++ electron/src/services/uhk-device.service.ts
@@ -99,7 +99,7 @@ export class UhkDeviceService implements OnDestroy {
         }
 
         // https://github.com/tessel/node-usb/issues/30
-        // Mac is not allow excusive right to use USB
+        // Mac is not allow exclusive right to use USB
         if (process.platform !== 'darwin') {
             usbInterface.claim();
         }
