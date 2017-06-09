de39be3513207b4617ddcbec92dea269a12922a7
Róbert Kiss <ert78gb@gmail.com>
2017-06-10 01:40:09 +0200

    fix(log): Allowed transport level change only in main process

--- electron/src/services/electron-log.service.ts
+++ electron/src/services/electron-log.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as log from 'electron-log';
 
-log.transports.console.level = 'debug';
-log.transports.file.level = 'debug';
-
 import { ILogService } from '../../../shared/src/services/logger.service';
 
 /**
