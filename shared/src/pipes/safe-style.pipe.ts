fefe2b4877962d54b2ebf3d5cf9d44f3dd239acc
József Farkas <fjozsef001@gmail.com>
2017-06-12 21:03:16 +0200

    Add missing styles

---
+++ shared/src/pipes/safe-style.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { SafeStyle, DomSanitizer} from '@angular/platform-browser';
+
+@Pipe({
+    name: 'safeStyle'
+})
+export class SafeStylePipe implements PipeTransform {
+
+    constructor(private sanitizer: DomSanitizer) {}
+
+    transform(style: string): SafeStyle {
+        return this.sanitizer.bypassSecurityTrustStyle(style);
+    }
+}
