536f8025f609051346301bd14cdfd338bdc9765b
Róbert Kiss <ert78gb@gmail.com>
2017-06-13 13:00:34 +0200

    fix(store): Add missing rxjs imports to keymap effect

--- shared/src/store/effects/keymap.ts
+++ shared/src/store/effects/keymap.ts
@@ -7,6 +7,8 @@ import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/withLatestFrom';
 import 'rxjs/add/observable/of';
 
