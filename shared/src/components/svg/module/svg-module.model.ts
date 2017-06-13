c9cf2268949b97ed107965924318780380c78683
József Farkas <fjozsef001@gmail.com>
2017-06-13 22:21:30 +0200

    Calculate indices from ids

--- shared/src/components/svg/module/svg-module.model.ts
+++ shared/src/components/svg/module/svg-module.model.ts
@@ -1,16 +1,20 @@
 import { SvgKeyboardKey } from '../keys';
 
 export class SvgModule {
-    private coverages: any[];
-    private keyboardKeys: SvgKeyboardKey[];
-    private attributes: any;
+    coverages: any[];
+    keyboardKeys: SvgKeyboardKey[];
+    attributes: any;
 
     constructor(obj: { rect: any[], path: any[], $: Object }) {
-        this.keyboardKeys = obj.rect.map(rect => rect.$).map(rect => {
-            rect.height = +rect.height;
-            rect.width = +rect.width;
-            return rect;
-        });
+        let index: number;
+        const keys = obj.rect.map(rect => rect.$);
+        this.keyboardKeys = [];
+        for (let i = 0; i < keys.length; ++i) {
+            index = keys[i].id.slice(4) - 1; // remove 'key-' then switch to index from 0
+            keys[i].height = +keys[i].height;
+            keys[i].width = +keys[i].width;
+            this.keyboardKeys[index] = keys[i];
+        }
         this.coverages = obj.path;
         this.attributes = obj.$;
     }
