a30f1345ecb9ae60c5fba3536bc1b17460e7c97d
Róbert Kiss <ert78gb@gmail.com>
2017-06-17 16:24:57 +0200

    fix(auto-update): Add types to the event methods

--- electron/src/services/app-update-renderer.service.ts
+++ electron/src/services/app-update-renderer.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Store, Action } from '@ngrx/store';
+import { Action, Store } from '@ngrx/store';
 import { ipcRenderer } from 'electron';
+
 import { IpcEvents } from '../shared/util';
 import { AppState } from '../store';
 import { UpdateDownloadedAction } from '../store/actions/app-update.action';
 
+/**
+ * This service handle the application update events in the electron rendrel process.
+ *
+ * The class contains parameters with 'any' type, because the relevant type definitions in
+ * import { ProgressInfo } from 'electron-builder-http/out/ProgressCallbackTransform';
+ * import { VersionInfo } from 'electron-builder-http/out/publishOptions';
+ * but, typescript allow import these if import 'electron-updater' too, but I i don't want to import
+ * the updater in renderer process.
+ */
 @Injectable()
 export class AppUpdateRendererService {
     constructor(private store: Store<AppState>) {
@@ -20,33 +30,34 @@ export class AppUpdateRendererService {
     }
 
     private registerEvents() {
-        ipcRenderer.on(IpcEvents.autoUpdater.updateAvailable, (event, arg) => {
+        ipcRenderer.on(IpcEvents.autoUpdater.updateAvailable, (event: string, arg: any) => {
             this.writeUpdateState(IpcEvents.autoUpdater.updateAvailable, arg);
         });
 
-        ipcRenderer.on(IpcEvents.autoUpdater.updateNotAvailable, (event, arg) => {
-            this.writeUpdateState(IpcEvents.autoUpdater.updateNotAvailable, arg);
+        ipcRenderer.on(IpcEvents.autoUpdater.updateNotAvailable, () => {
+            this.writeUpdateState(IpcEvents.autoUpdater.updateNotAvailable);
         });
 
-        ipcRenderer.on(IpcEvents.autoUpdater.autoUpdateError, (event, arg) => {
+        ipcRenderer.on(IpcEvents.autoUpdater.autoUpdateError, (event: string, arg: any) => {
             this.writeUpdateState(IpcEvents.autoUpdater.autoUpdateError, arg);
         });
 
-        ipcRenderer.on(IpcEvents.autoUpdater.autoUpdateDownloadProgress, (event, arg) => {
+        ipcRenderer.on(IpcEvents.autoUpdater.autoUpdateDownloadProgress, (event: string, arg: any) => {
             this.writeUpdateState(IpcEvents.autoUpdater.autoUpdateDownloadProgress, arg);
         });
 
-        ipcRenderer.on(IpcEvents.autoUpdater.autoUpdateDownloaded, (event, arg) => {
+        ipcRenderer.on(IpcEvents.autoUpdater.autoUpdateDownloaded, (event: string, arg: any) => {
             this.writeUpdateState(IpcEvents.autoUpdater.autoUpdateDownloaded, arg);
             this.dispachStoreAction(new UpdateDownloadedAction());
         });
 
     }
+
     private dispachStoreAction(action: Action) {
         this.store.dispatch(action);
     }
 
-    private writeUpdateState(event: any, arg: any) {
+    private writeUpdateState(event: any, arg?: any) {
         console.log({ event, arg });
     }
 }
