a76624d5d6d06fd01fec17c195aef94e4e5af167
Róbert Kiss <ert78gb@gmail.com>
2017-07-05 10:56:17 +0200

    fix(auto-update): Add missing rxjs imports

--- electron/src/store/effects/app-update.effect.ts
+++ electron/src/store/effects/app-update.effect.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { Action } from '@ngrx/store';
 import { Actions, Effect, toPayload } from '@ngrx/effects';
 import { Observable } from 'rxjs/Observable';
+
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/first';
 
 import { ActionTypes } from '../../shared/store/actions/app-update.action';
