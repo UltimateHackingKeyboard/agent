7982a0184b59d482646093d527408ab30a2c231f
József Farkas <fjozsef001@gmail.com>
2017-07-09 13:23:43 +0200

    Update tslint

--- electron/src/services/electron-error-handler.service.ts
+++ electron/src/services/electron-error-handler.service.ts
@@ -1,9 +1,9 @@
 import { ErrorHandler, Injectable } from '@angular/core';
-import { LogService} from '../shared/services/logger.service';
+import { LogService } from '../shared/services/logger.service';
 
 @Injectable()
 export class ElectronErrorHandlerService implements ErrorHandler {
-    constructor(private logService: LogService) {}
+    constructor(private logService: LogService) { }
 
     handleError(error: any) {
         this.logService.error(error);
