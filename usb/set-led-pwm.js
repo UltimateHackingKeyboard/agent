eb16c422e58e6ca7d5f1019c2d588cee4d57ff45
László Monda <laci@monda.hu>
2017-08-13 14:22:04 +0200

    Fix script.

--- usb/set-led-pwm.js
+++ usb/set-led-pwm.js
@@ -3,15 +3,13 @@ let uhk = require('./uhk');
 
 let programName = process.argv[1];
 
-if (process.argv.length !== 4) {
-    console.log(`Usage: ${programName} [left LED pwm 0-100] [right LED pwm 0-100]`);
+if (process.argv.length !== 3) {
+    console.log(`Usage: ${programName} [LED pwm 0-100]`);
     process.exit(1);
 }
 
 let leftBrightnessPercent = process.argv[2] || '';
-let rightBrightnessPercent = process.argv[3] || '';
 
 uhk.sendUsbPackets([
-    new Buffer([uhk.usbCommands.setLedPwm, 0, +leftBrightnessPercent]),
-    new Buffer([uhk.usbCommands.setLedPwm, 1, +rightBrightnessPercent]),
+    new Buffer([uhk.usbCommands.setLedPwm, +leftBrightnessPercent]),
 ]);
