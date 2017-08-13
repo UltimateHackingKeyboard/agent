496a020d13c7d66e1c7996bae17b5c46fa476de7
Róbert Kiss <ert78gb@gmail.com>
2017-08-13 02:50:16 +0200

    turn back electron storage service

--- packages/uhk-web/src/renderer/uhk-renderer.module.ts
+++ packages/uhk-web/src/renderer/uhk-renderer.module.ts
@@ -1,18 +1,15 @@
 import { ErrorHandler, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogService } from 'uhk-common';
-// import { ElectronDataStorageRepositoryService } from './services/electron-datastorage-repository.service';
+import { ElectronDataStorageRepositoryService } from './services/electron-datastorage-repository.service';
 import { ElectronLogService } from './services/electron-log.service';
 import { ElectronErrorHandlerService } from './services/electron-error-handler.service';
-import { UhkHidApiService } from './services/uhk-hid-api.service';
-import { uhkDeviceFactory } from './services/uhk-device-provider';
-import { UhkDeviceService } from './services/uhk-device.service';
 import { WebModule } from '../app/web.module';
 import { routing } from '../app/app.routes';
 import { MainAppComponent } from '../app/app.component';
 import { IpcUhkRenderer } from './services/ipc-uhk-renderer';
 import { IpcCommonRenderer } from '../app/services/ipc-common-renderer';
-// import { DATA_STORAGE_REPOSITORY } from './services/datastorage-repository.service';
+import { DataStorageRepositoryService } from '../app/services/datastorage-repository.service';
 
 @NgModule({
   imports: [
@@ -21,7 +18,7 @@ import { IpcCommonRenderer } from '../app/services/ipc-common-renderer';
     routing
   ],
   providers: [
-    // { provide: DATA_STORAGE_REPOSITORY, useClass: ElectronDataStorageRepositoryService },
+    { provide: DataStorageRepositoryService, useClass: ElectronDataStorageRepositoryService },
     { provide: IpcCommonRenderer, useClass: IpcUhkRenderer },
     { provide: LogService, useClass: ElectronLogService },
     { provide: ErrorHandler, useClass: ElectronErrorHandlerService }
