c954c6bf510412b5b60da0790a64144c88e14c21
László Monda <laci@monda.hu>
2017-08-30 20:54:53 +0200

    Fix filename.

--- packages/usb/update-right-firmware.sh
+++ packages/usb/update-right-firmware.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e # fail the script if a command fails
 
-./jump-to-bootloader.sh
+./jump-to-bootloader.js
 blhost --usb 0x15a2,0x0073 flash-security-disable 0403020108070605
 blhost --usb 0x15a2,0x0073 flash-erase-region 0xc000 475136
 blhost --usb 0x15a2,0x0073 flash-image uhk-right.srec
