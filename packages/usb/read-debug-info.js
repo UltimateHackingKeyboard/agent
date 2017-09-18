1122784bdbddfffbd1c1fd72997648a6f924b7bb
László Monda <laci@monda.hu>
2017-09-19 01:08:53 +0200

    Fix script.

--- packages/usb/read-debug-info.js
+++ packages/usb/read-debug-info.js
@@ -6,7 +6,6 @@ function readDebugInfo() {
     const payload = new Buffer([uhk.usbCommands.readDebugInfo]);
     console.log('Sending ', uhk.bufferToString(payload));
     device.write(uhk.getTransferData(payload));
-    device.write([64]);
     const receivedBuffer = Buffer.from(device.readSync());
     console.log('Received', uhk.bufferToString(receivedBuffer));
     setTimeout(readDebugInfo, 500)
