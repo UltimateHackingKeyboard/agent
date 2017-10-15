bff97f02d7121efc36e6a65c372f6ce309daef1c
László Monda <laci@monda.hu>
2017-10-15 16:05:47 +0200

    Change "Loading Agent..." to "Loading keyboard configuration..."

--- packages/uhk-web/src/app/pages/loading-page/loading-device.page.ts
+++ packages/uhk-web/src/app/pages/loading-page/loading-device.page.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'loading-device',
     template: `
-        <uhk-message title="Loading Agent..."
+        <uhk-message title="Loading keyboard configuration..."
                      subtitle="Hang tight!"
                      [rotateLogo]="true"></uhk-message>
     `
