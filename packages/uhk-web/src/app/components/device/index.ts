b14c3434a1555c6157c4caac94d5c973350709c1
Róbert Kiss <ert78gb@gmail.com>
2017-10-17 23:40:11 +0200

    feat(agent): Add 'Device' menu and submenus

--- packages/uhk-web/src/app/components/device/index.ts
+++ packages/uhk-web/src/app/components/device/index.ts
@@ -1,2 +1,4 @@
-export * from './settings/device-settings.component';
+export * from './configuration/device-configuration.component';
+export * from './firmware/device-firmware.component';
+export * from './mouse-speed/mouse-speed.component';
 export * from './device.routes';
---
+++ packages/uhk-web/src/app/components/device/mouse-speed/mouse-speed.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'device-mouse-speed',
+    templateUrl: './mouse-speed.component.html',
+    styleUrls: ['./mouse-speed.component.scss'],
+    host: {
+        'class': 'container-fluid'
+    }
+})
+export class MouseSpeedComponent {
+}
