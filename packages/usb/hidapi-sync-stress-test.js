3f78cc13404886f3879ad3b73eec43115ff2f70b
László Monda <laci@monda.hu>
2017-10-31 20:08:33 +0100

    Add script to test hidapi stability by using readSync() calls.

---
+++ packages/usb/hidapi-sync-stress-test.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const uhk = require('./uhk');
+
+const device = uhk.getUhkDevice();
+let counter = 1;
+
+while (true) {
+    console.log(`hidapi sync test ${counter++}`);
+    const sendData = new Buffer([uhk.usbCommands.getProperty, uhk.systemPropertyIds.hardwareConfigSize]);
+    device.write(uhk.getTransferData(sendData));
+    device.readSync()
+}
