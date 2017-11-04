7e4edf3d84287f13adc00490b15c2ee3b8100aae
Róbert Kiss <ert78gb@gmail.com>
2017-11-04 11:02:00 +0100

    make run able ts file from other directory

--- packages/usb/user-config-json-to-bin.ts
+++ packages/usb/user-config-json-to-bin.ts
@@ -1,10 +1,12 @@
 #!/usr/bin/env ts-node
+///<reference path="./node_modules/@types/node/index.d.ts"/>
 
 import { UhkBuffer, UserConfiguration } from 'uhk-common';
 import * as fs from 'fs';
+import * as path from 'path';
 
 const outputFile = process.argv[2];
-const inputFile = `${__dirname}/../uhk-web/src/app/services/user-config.json`;
+const inputFile = path.join(__dirname, '/../uhk-web/src/app/services/user-config.json');
 const config1Js = JSON.parse(fs.readFileSync(inputFile).toString());
 const config1Ts: UserConfiguration = new UserConfiguration().fromJsonObject(config1Js);
 const config1Buffer = new UhkBuffer();
