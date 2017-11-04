a5c478d51c1cefa0f4281f662ea1087bf3cebf53
László Monda <laci@monda.hu>
2017-11-04 17:45:26 +0100

    Remove the unused scripts section of package.json and make user-config-json-to-bin.ts slightly simpler.

--- packages/usb/user-config-json-to-bin.ts
+++ packages/usb/user-config-json-to-bin.ts
@@ -3,10 +3,9 @@
 
 import { UhkBuffer, UserConfiguration } from 'uhk-common';
 import * as fs from 'fs';
-import * as path from 'path';
 
 const outputFile = process.argv[2];
-const inputFile = path.join(__dirname, '/../uhk-web/src/app/services/user-config.json');
+const inputFile = `${__dirname}/../uhk-web/src/app/services/user-config.json`;
 const config1Js = JSON.parse(fs.readFileSync(inputFile).toString());
 const config1Ts: UserConfiguration = new UserConfiguration().fromJsonObject(config1Js);
 const config1Buffer = new UhkBuffer();
