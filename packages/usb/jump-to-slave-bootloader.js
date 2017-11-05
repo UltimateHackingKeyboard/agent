5252ec3702eee8f725687653400729290359eb20
László Monda <laci@monda.hu>
2017-11-06 00:29:24 +0100

    Increment the argument of jumpToSlaveBootloader now that it's been changed from UHK module driver id to slot id.

--- packages/usb/jump-to-slave-bootloader.js
+++ packages/usb/jump-to-slave-bootloader.js
@@ -2,6 +2,6 @@
 const uhk = require('./uhk');
 
 const device = uhk.getUhkDevice();
-let transfer = new Buffer([uhk.usbCommands.jumpToSlaveBootloader, 0]);
+let transfer = new Buffer([uhk.usbCommands.jumpToSlaveBootloader, 1]);
 device.write(uhk.getTransferData(transfer));
 const response = Buffer.from(device.readSync());
