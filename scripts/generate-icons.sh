ebdb0b114c8dbb12e334e48220aa9d7afe48dfb5
László Monda <laci@monda.hu>
2017-11-07 23:56:28 +0100

    Add icon generator script.

---
+++ scripts/generate-icons.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# apt-get install inkscape imagemagick-6.q16 icnsutils
+
+agentIconSvg=../packages/uhk-web/src/svgs/keyboard/icons/agent-icon.svg
+
+for size in 16 24 32 48 64 96 128 256 512 1024; do
+    inkscape -z --export-png=../build/icons/${size}x${size}.png -w $size $agentIconSvg
+done
+
+convert ../build/icons/256x256.png ../build/icon.ico
+
+cd ../build/icons
+png2icns ../icon.icns 16x16.png 32x32.png 48x48.png 128x128.png 256x256.png 512x512.png
