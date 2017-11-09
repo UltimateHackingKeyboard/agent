a2fc30175d66aded347deaaff35f82350d3a72c4
László Monda <laci@monda.hu>
2017-11-09 18:59:04 +0100

    Dump individual debug values in an easy to read fashion.

--- packages/usb/get-debug-info.js
+++ packages/usb/get-debug-info.js
@@ -2,12 +2,21 @@
 const uhk = require('./uhk');
 const device = uhk.getUhkDevice();
 
+function getUint32(buffer, offset) {
+    return (buffer[offset]) + (buffer[offset+1] << 8) + (buffer[offset+2] << 16) + (buffer[offset+3] << 24);
+}
+
 function getDebugInfo() {
-    const payload = new Buffer([uhk.usbCommands.readDebugInfo]);
+    const payload = new Buffer([uhk.usbCommands.getDebugInfo]);
     console.log('Sending ', uhk.bufferToString(payload));
     device.write(uhk.getTransferData(payload));
-    const receivedBuffer = Buffer.from(device.readSync());
-    console.log('Received', uhk.bufferToString(receivedBuffer));
+    const rxBuffer = Buffer.from(device.readSync());
+    console.log('Received', uhk.bufferToString(rxBuffer));
+    process.stdout.write(`I2C_Watchdog:${getUint32(rxBuffer, 1)} | `);
+    process.stdout.write(`I2cSchedulerCounter:${getUint32(rxBuffer, 5)} | `);
+    process.stdout.write(`I2cWatchdog_OuterCounter:${getUint32(rxBuffer, 9)} | `);
+    process.stdout.write(`I2cWatchdog_InnerCounter:${getUint32(rxBuffer, 13)}`);
+    process.stdout.write('\n');
     setTimeout(getDebugInfo, 500);
 }
 
