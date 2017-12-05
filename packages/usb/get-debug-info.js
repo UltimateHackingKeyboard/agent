f44202c0e764d9681e91405200ca9d175e4d9c48
László Monda <laci@monda.hu>
2017-12-05 03:05:20 +0100

    Extract debug values to constants and compute USB interface callbacks per second.

--- packages/usb/get-debug-info.js
+++ packages/usb/get-debug-info.js
@@ -10,19 +10,55 @@ function getUint16(buffer, offset) {
     return (buffer[offset]) + (buffer[offset+1] << 8);
 }
 
+let prevGeneric, prevBasic, prevMedia, prevSystem, prevMouse;
 function getDebugInfo() {
+
     const payload = new Buffer([uhk.usbCommands.getDebugInfo]);
     console.log('Sending ', uhk.bufferToString(payload));
     device.write(uhk.getTransferData(payload));
     const rxBuffer = Buffer.from(device.readSync());
     console.log('Received', uhk.bufferToString(rxBuffer));
-    process.stdout.write(`I2C_Watchdog:${getUint32(rxBuffer, 1)} | `);
-    process.stdout.write(`I2cSlaveScheduler_Counter:${getUint32(rxBuffer, 5)} | `);
-    process.stdout.write(`I2cWatchdog_WatchCounter:${getUint32(rxBuffer, 9)} | `);
-    process.stdout.write(`I2cWatchdog_RecoveryCounter:${getUint32(rxBuffer, 13)} | `);
-    process.stdout.write(`Timer:${getUint32(rxBuffer, 40)}`);
+
+    const I2C_Watchdog = getUint32(rxBuffer, 1);
+    const I2cSlaveScheduler_Counter = getUint32(rxBuffer, 5);
+    const I2cWatchdog_WatchCounter = getUint32(rxBuffer, 9);
+    const I2cWatchdog_RecoveryCounter = getUint32(rxBuffer, 13);
+    const KeyScannerCounter = getUint32(rxBuffer, 17);
+    const UsbReportUpdateCounter = getUint32(rxBuffer, 21);
+    const CurrentTime = getUint32(rxBuffer, 25);
+    const UsbGenericHidActionCounter = getUint32(rxBuffer, 29);
+    const UsbBasicKeyboardActionCounter = getUint32(rxBuffer, 33);
+    const UsbMediaKeyboardActionCounter = getUint32(rxBuffer, 37);
+    const UsbSystemKeyboardActionCounter = getUint32(rxBuffer, 41);
+    const UsbMouseActionCounter = getUint32(rxBuffer, 45);
+
+    process.stdout.write(`I2C_Watchdog:${I2C_Watchdog} | `);
+    process.stdout.write(`I2cSlaveScheduler_Counter:${I2cSlaveScheduler_Counter} | `);
+    process.stdout.write(`I2cWatchdog_WatchCounter:${I2cWatchdog_WatchCounter} | `);
+    process.stdout.write(`I2cWatchdog_RecoveryCounter:${I2cWatchdog_RecoveryCounter} | `);
+    process.stdout.write(`KeyScannerCounter:${KeyScannerCounter} | `);
+    process.stdout.write(`UsbReportUpdateCounter:${UsbReportUpdateCounter} | `);
+    process.stdout.write(`CurrentTime:${CurrentTime} | `);
+    process.stdout.write(`UsbGenericHidActionCounter:${UsbGenericHidActionCounter} | `);
+    process.stdout.write(`UsbBasicKeyboardActionCounter:${UsbBasicKeyboardActionCounter} | `);
+    process.stdout.write(`UsbMediaKeyboardActionCounter:${UsbMediaKeyboardActionCounter} | `);
+    process.stdout.write(`UsbSystemKeyboardActionCounter:${UsbSystemKeyboardActionCounter} | `);
+    process.stdout.write(`UsbMouseActionCounter:${UsbMouseActionCounter} | `);
     process.stdout.write('\n');
-    setTimeout(getDebugInfo, 500);
+
+    process.stdout.write(`generic:${UsbGenericHidActionCounter - prevGeneric} `)
+    process.stdout.write(`basic:${UsbBasicKeyboardActionCounter - prevBasic} `)
+    process.stdout.write(`basic:${UsbMediaKeyboardActionCounter - prevMedia} `)
+    process.stdout.write(`basic:${UsbSystemKeyboardActionCounter - prevSystem} `)
+    process.stdout.write(`basic:${UsbMouseActionCounter - prevMouse} `)
+
+    prevGeneric = UsbGenericHidActionCounter;
+    prevBasic = UsbBasicKeyboardActionCounter;
+    prevMedia = UsbMediaKeyboardActionCounter;
+    prevSystem = UsbSystemKeyboardActionCounter;
+    prevMouse = UsbMouseActionCounter;
+
+    setTimeout(getDebugInfo, 1000);
 }
 
 getDebugInfo();
