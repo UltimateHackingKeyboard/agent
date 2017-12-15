969c36561b29b12039f41b234146cd3ae9ab86a9
László Monda <laci@monda.hu>
2017-12-15 04:33:10 +0100

    Fix script name.

--- packages/usb/update-loop-getkey.sh
+++ packages/usb/update-loop-getkey.sh
@@ -5,8 +5,8 @@ firmwarePath=$1
 while true; do
     read -n 1 char
     if [[ $char == "a" ]]; then
-        ./flash update-firmwares-and-configs.js "$firmwarePath" ansi
+        ./update-firmwares-and-configs.js "$firmwarePath" ansi
     elif [[ $char == "i" ]]; then
-        ./flash update-firmwares-and-configs.js "$firmwarePath" iso
+        ./update-firmwares-and-configs.js "$firmwarePath" iso
     fi
 done
