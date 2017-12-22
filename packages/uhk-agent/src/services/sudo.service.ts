ba1ede1848448a56a2c46ab2709ceb2ee3838848
Róbert Kiss <ert78gb@gmail.com>
2017-12-22 18:01:37 +0100

    fix(device): use tmp path to run setup-rules.sh

--- packages/uhk-agent/src/services/sudo.service.ts
+++ packages/uhk-agent/src/services/sudo.service.ts
@@ -2,6 +2,8 @@ import { ipcMain, app } from 'electron';
 import * as isDev from 'electron-is-dev';
 import * as path from 'path';
 import * as sudo from 'sudo-prompt';
+import { dirSync } from 'tmp';
+import { emptyDir, copy } from 'fs-extra';
 
 import { IpcEvents, LogService, IpcResponse } from 'uhk-common';
 
@@ -18,10 +20,10 @@ export class SudoService {
         ipcMain.on(IpcEvents.device.setPrivilegeOnLinux, this.setPrivilege.bind(this));
     }
 
-    private setPrivilege(event: Electron.Event) {
+    private async setPrivilege(event: Electron.Event) {
         switch (process.platform) {
             case 'linux':
-                this.setPrivilegeOnLinux(event);
+                await this.setPrivilegeOnLinux(event);
                 break;
             default:
                 const response: IpcResponse = {
@@ -34,14 +36,19 @@ export class SudoService {
         }
     }
 
-    private setPrivilegeOnLinux(event: Electron.Event) {
+    private async setPrivilegeOnLinux(event: Electron.Event) {
         const scriptPath = path.join(this.rootDir, 'rules/setup-rules.sh');
+        const tmpDirectory = dirSync();
+        const tmpSetupRulesPath = path.join(tmpDirectory.name, 'setup-rules.sh');
+        this.logService.debug('[SudoService] Copy setup-rules.sh', { src: scriptPath, dst: tmpSetupRulesPath });
+        await copy(scriptPath, tmpSetupRulesPath);
+
         const options = {
             name: 'Setting UHK access rules'
         };
-        const command = `sh ${scriptPath}`;
+        const command = `sh ${tmpSetupRulesPath}`;
         this.logService.debug('[SudoService] Set privilege command: ', command);
-        sudo.exec(command, options, (error: any) => {
+        sudo.exec(command, options, async (error: any) => {
             const response = new IpcResponse();
 
             if (error) {
@@ -52,6 +59,7 @@ export class SudoService {
                 response.success = true;
             }
 
+            await emptyDir(tmpDirectory.name);
             event.sender.send(IpcEvents.device.setPrivilegeOnLinuxReply, response);
         });
     }
