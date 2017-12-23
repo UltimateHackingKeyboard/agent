165407ddc23c1ae78f29dad9b8c9443c5e9d022d
Mikko Lakomaa <hopealusikka@gmail.com>
2017-12-24 00:58:41 +0200

    Various tweaks to LEDBrightnessComponent

--- packages/uhk-web/src/app/components/device/led-brightness/led-brightness.component.ts
+++ packages/uhk-web/src/app/components/device/led-brightness/led-brightness.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChildren, QueryList } from '@angular/core';
 
 import { Store } from '@ngrx/store';
 import { AppState } from '../../../store/index';
+import { NouisliderComponent } from 'ng2-nouislider/src/nouislider';
+
+const sliderPips = {
+	mode: 'positions',
+    values: [0, 50, 128, 255],
+    density: 6,
+    stepped: true
+}
 
 @Component({
     selector: 'device-led-brightness',
@@ -11,10 +19,27 @@ import { AppState } from '../../../store/index';
         'class': 'container-fluid'
     }
 })
-export class LEDBrightnessComponent {
+export class LEDBrightnessComponent implements AfterViewInit {
+    @ViewChildren(NouisliderComponent) sliders: QueryList<NouisliderComponent>;
+
     public iconsAndLayerTextsBrightness: number = 0;
     public alphanumericSegmentsBrightness: number = 0;
     public keyBacklightBrightness: number = 0;
+    public sliderTooltipsEnabled: boolean = false;
 
     constructor(private store: Store<AppState>) {}
+
+    ngAfterViewInit() {
+        this.sliders.forEach(slider => {
+            slider.slider.pips(sliderPips);
+            slider.slider.target.querySelector('.noUi-tooltip').style.display = 'none';
+            
+            slider.slider.on('start', function() {
+                this.target.querySelector('.noUi-tooltip').style.display = 'block';
+            });
+            slider.slider.on('end', function() {
+                this.target.querySelector('.noUi-tooltip').style.display = 'none';
+            });
+        });
+    }
 }
