bd5be98d997d40a0557bf22f67a17f6e69e9c18d
László Monda <laci@monda.hu>
2018-01-08 06:13:37 +0100

    Restore wdi-simple.exe and 50-uhk60-rules.cmd just in case.

---
+++ packages/usb/zadic/50-uhk60-rules.cmd
@@ -0,0 +1,4 @@
+wdi-simple.exe --vid 0x1d50 --pid  0x6120 --iid 0 --type 1 --silent
+wdi-simple.exe --vid 0x1d50 --pid  0x6121 --iid 0 --type 1 --silent
+wdi-simple.exe --vid 0x1d50 --pid  0x6122 --iid 0 --type 1 --silent
+wdi-simple.exe --vid 0x1d50 --pid  0x6123 --iid 0 --type 1 --silent
