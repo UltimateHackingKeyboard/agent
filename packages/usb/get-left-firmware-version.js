1aeb4e832618512f3bdeb1be5bfd746cc4802842
László Monda <laci@monda.hu>
2018-01-15 00:07:30 +0100

    Make get-left-firmware-version.js display module protocol version, too.

--- packages/usb/get-left-firmware-version.js
+++ packages/usb/get-left-firmware-version.js
@@ -7,10 +7,12 @@ console.log(sendData)
 device.write(uhk.getTransferData(sendData));
 const response = Buffer.from(device.readSync());
 console.log(response)
+const moduleProtocolMajorVersion = uhk.getUint16(response, 2);
+const moduleProtocolMinorVersion = uhk.getUint16(response, 4);
+const moduleProtocolPatchVersion = uhk.getUint16(response, 6);
 const firmwareMajorVersion = uhk.getUint16(response, 8);
 const firmwareMinorVersion = uhk.getUint16(response, 10);
 const firmwarePatchVersion = uhk.getUint16(response, 12);
 
-console.log(`firmwareMajorVersion: ${firmwareMajorVersion}`);
-console.log(`firmwareMinorVersion: ${firmwareMinorVersion}`);
-console.log(`firmwarePatchVersion: ${firmwarePatchVersion}`);
+console.log(`moduleProtocolVersion: ${moduleProtocolMajorVersion}.${moduleProtocolMinorVersion}.${moduleProtocolPatchVersion}`);
+console.log(`firmwareVersion: ${firmwareMajorVersion}.${firmwareMinorVersion}.${firmwarePatchVersion}`);
