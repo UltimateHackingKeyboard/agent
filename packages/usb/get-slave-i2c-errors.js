010a23aaeb2a231466448d28e2d4cc0e4c53bdb1
László Monda <laci@monda.hu>
2018-01-15 01:07:23 +0100

    Extract slaveI2cErrorBufferToString()

--- packages/usb/get-slave-i2c-errors.js
+++ packages/usb/get-slave-i2c-errors.js
@@ -17,35 +17,40 @@ if (status != 0) {
     process.exit(1);
 }
 
-let statusCount = response[1];
+function slaveI2cErrorBufferToString(buffer) {
+    let statusCount = buffer[1];
 
-const slaveIdToName = [
-    'leftHalf',
-    'leftAddon',
-    'rightAddon',
-    'rightLedDriver',
-    'leftLedDriver',
-    'kboot',
-];
+    const slaveIdToName = [
+        'leftHalf',
+        'leftAddon',
+        'rightAddon',
+        'rightLedDriver',
+        'leftLedDriver',
+        'kboot',
+    ];
 
-let str = `${slaveIdToName[slaveId].padEnd(14)}: `;
+    let str = `${slaveIdToName[slaveId].padEnd(14)}: `;
 
-const statusCodesToStrings = {
-    0: 'nak',
-    1: 'failure',
-    1100: 'busy',
-    1101: 'idle',
-    1102: 'nak',
-    1103: 'arbitrationLost',
-    1104: 'timeout',
-    20000: 'idleSlave',
-    20001: 'idleCycle',
-};
+    const statusCodesToStrings = {
+        0: 'nak',
+        1: 'failure',
+        1100: 'busy',
+        1101: 'idle',
+        1102: 'nak',
+        1103: 'arbitrationLost',
+        1104: 'timeout',
+        20000: 'idleSlave',
+        20001: 'idleCycle',
+    };
 
-for (let i=0; i<statusCount; i++) {
-    let status = uhk.getUint32(response, i*8+2);
-    let count = uhk.getUint32(response, i*8+4+2);
-    str += `${statusCodesToStrings[status]}:${count} `;
+    for (let i=0; i<statusCount; i++) {
+        let status = uhk.getUint32(buffer, i*8+2);
+        let count = uhk.getUint32(buffer, i*8+4+2);
+        str += `${statusCodesToStrings[status]}:${count} `;
+    }
+
+    return str;
 }
 
+let str = slaveI2cErrorBufferToString(response);
 console.log(str);
