5476f7c3a5608810264144973f630f71555e0b77
László Monda <laci@monda.hu>
2018-01-30 17:25:29 +0100

    Extract I2C0_F.

--- packages/usb/get-i2c-baud-rate.js
+++ packages/usb/get-i2c-baud-rate.js
@@ -6,5 +6,6 @@ const device = uhk.getUhkDevice();
     let response = await uhk.writeDevice(device, [uhk.usbCommands.getDeviceProperty, uhk.devicePropertyIds.i2cBaudRate]);
     let requestedBaudRate = uhk.getUint32(response, 2);
     let actualBaudRate = uhk.getUint32(response, 6);
-    console.log(`requestedBaudRate:${requestedBaudRate} | actualBaudRate:${actualBaudRate} | I2C0_F:0b${response[1].toString(2).padStart(8, '0')}`)
+    let i2c0F = response[1].toString(2).padStart(8, '0');
+    console.log(`requestedBaudRate:${requestedBaudRate} | actualBaudRate:${actualBaudRate} | I2C0_F:0b${i2c0F}`)
 })();
