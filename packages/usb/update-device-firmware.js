ca9bf60a1becce063ff74724bbd24ec3b8f24f10
László Monda <laci@monda.hu>
2018-01-30 21:46:39 +0100

    Extract updateDeviceFirmware()

--- packages/usb/update-device-firmware.js
+++ packages/usb/update-device-firmware.js
@@ -3,26 +3,30 @@ const uhk = require('./uhk');
 const program = require('commander');
 require('shelljs/global');
 
-const extension = '.hex';
 config.fatal = true;
+const extension = '.hex';
 
 program
     .usage(`firmwareImage${extension}`)
     .parse(process.argv)
 
 const firmwareImage = program.args[0];
-const usbDir = `${__dirname}`;
-const blhost = uhk.getBlhostCmd(uhk.enumerationNameToProductId.bootloader);
 
-uhk.checkFirmwareImage(firmwareImage, extension);
+async function updateDeviceFirmware(firmwareImage, extension) {
+    const usbDir = `${__dirname}`;
+    const blhost = uhk.getBlhostCmd(uhk.enumerationNameToProductId.bootloader);
 
-(async function() {
+    uhk.checkFirmwareImage(firmwareImage, extension);
     config.verbose = true;
+
     await uhk.reenumerate('bootloader');
     exec(`${blhost} flash-security-disable 0403020108070605`);
     exec(`${blhost} flash-erase-region 0xc000 475136`);
     exec(`${blhost} flash-image ${firmwareImage}`);
     exec(`${blhost} reset`);
+
     config.verbose = false;
     echo('Firmware updated successfully.');
-})();
+};
+
+updateDeviceFirmware(firmwareImage, extension);
