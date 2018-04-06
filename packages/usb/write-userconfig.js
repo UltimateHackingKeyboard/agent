026d217d02d141a9ac6c7b09cff0f009621a4684
László Monda <laci@monda.hu>
2018-04-06 13:59:37 +0200

    Add write-userconfig.js and invalid-config.bin

---
+++ packages/usb/write-userconfig.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const program = require('commander');
+const uhk = require('./uhk');
+
+(async function() {
+    program
+        .usage(`configPath`)
+        .parse(process.argv);
+
+    if (program.args.length < 1) {
+        console.error('No configPath path specified.');
+        exit(1);
+    }
+    const configPath = program.args[0];
+
+    const device = uhk.getUhkDevice();
+    const configBuffer = fs.readFileSync(configPath);
+    await uhk.writeConfig(device, configBuffer, false);
+    await uhk.launchEepromTransfer(device, uhk.eepromOperations.write, uhk.configBufferIds.stagingUserConfig);
+})();
