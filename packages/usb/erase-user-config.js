ea41661c65e1fadffea4a8c19b3bae4634b5ba21
László Monda <laci@monda.hu>
2018-05-19 21:53:41 +0200

    Add erase-user-config.js

---
+++ packages/usb/erase-user-config.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const uhk = require('./uhk');
+
+(async function() {
+    const device = uhk.getUhkDevice();
+    const buffer = new Buffer(Array(2**15-64).fill(0xff));
+    await uhk.writeConfig(device, buffer, false);
+    await uhk.launchEepromTransfer(device, uhk.eepromOperations.write, uhk.configBufferIds.stagingUserConfig);
+})();
