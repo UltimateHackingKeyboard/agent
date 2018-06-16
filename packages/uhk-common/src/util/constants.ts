41faa98fcd1a36bc6bf74f256b0e12c19513043d
László Monda <laci@monda.hu>
2018-06-16 18:22:01 +0200

    Update the firmware update instructions and URL.

--- packages/uhk-common/src/util/constants.ts
+++ packages/uhk-common/src/util/constants.ts
@@ -1,4 +1,4 @@
 export namespace Constants {
     export const AGENT_GITHUB_URL = 'https://github.com/UltimateHackingKeyboard/agent';
-    export const FIRMWARE_GITHUB_ISSUE_URL = 'https://github.com/UltimateHackingKeyboard/agent/issues/567';
+    export const FIRMWARE_GITHUB_ISSUE_URL = 'https://github.com/UltimateHackingKeyboard/agent/issues/new';
 }
