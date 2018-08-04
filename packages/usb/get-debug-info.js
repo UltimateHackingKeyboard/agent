67d42f666c0008921875ac5cf05ba730da269f0f
László Monda <laci@monda.hu>
2018-08-04 02:29:39 +0200

    Simplify the output of get-debug-info.js

--- packages/usb/get-debug-info.js
+++ packages/usb/get-debug-info.js
@@ -14,11 +14,11 @@ let prevGeneric, prevBasic, prevMedia, prevSystem, prevMouse;
 function getDebugInfo() {
 
     const payload = new Buffer([uhk.usbCommands.getDebugBuffer]);
-    console.log(payload)
-    console.log('Sending ', uhk.bufferToString(payload));
+//    console.log(payload)
+//    console.log('Sending ', uhk.bufferToString(payload));
     device.write(uhk.getTransferData(payload));
     const rxBuffer = Buffer.from(device.readSync());
-    console.log('Received', uhk.bufferToString(rxBuffer));
+//    console.log('Received', uhk.bufferToString(rxBuffer));
 
     const I2C_Watchdog = getUint32(rxBuffer, 1);
     const I2cSlaveScheduler_Counter = getUint32(rxBuffer, 5);
@@ -33,25 +33,25 @@ function getDebugInfo() {
     const UsbSystemKeyboardActionCounter = getUint32(rxBuffer, 41);
     const UsbMouseActionCounter = getUint32(rxBuffer, 45);
 
-    process.stdout.write(`I2C_Watchdog:${I2C_Watchdog} | `);
-    process.stdout.write(`I2cSlaveScheduler_Counter:${I2cSlaveScheduler_Counter} | `);
-    process.stdout.write(`I2cWatchdog_WatchCounter:${I2cWatchdog_WatchCounter} | `);
-    process.stdout.write(`I2cWatchdog_RecoveryCounter:${I2cWatchdog_RecoveryCounter} | `);
-    process.stdout.write(`KeyScannerCounter:${KeyScannerCounter} | `);
-    process.stdout.write(`UsbReportUpdateCounter:${UsbReportUpdateCounter} | `);
-    process.stdout.write(`CurrentTime:${CurrentTime} | `);
-    process.stdout.write(`UsbGenericHidActionCounter:${UsbGenericHidActionCounter} | `);
-    process.stdout.write(`UsbBasicKeyboardActionCounter:${UsbBasicKeyboardActionCounter} | `);
-    process.stdout.write(`UsbMediaKeyboardActionCounter:${UsbMediaKeyboardActionCounter} | `);
-    process.stdout.write(`UsbSystemKeyboardActionCounter:${UsbSystemKeyboardActionCounter} | `);
-    process.stdout.write(`UsbMouseActionCounter:${UsbMouseActionCounter} | `);
+    process.stdout.write(`I2cWatchdog:${I2C_Watchdog} | `);
+    process.stdout.write(`I2cSlave:${I2cSlaveScheduler_Counter} | `);
+    process.stdout.write(`I2cWatch:${I2cWatchdog_WatchCounter} | `);
+    process.stdout.write(`I2cRecovery:${I2cWatchdog_RecoveryCounter} | `);
+    process.stdout.write(`KeyMatrix:${KeyScannerCounter} | `);
+    process.stdout.write(`UsbReport:${UsbReportUpdateCounter} | `);
+    process.stdout.write(`Time:${CurrentTime} | `);
+    process.stdout.write(`UsbGeneric:${UsbGenericHidActionCounter} | `);
+    process.stdout.write(`UsbBasic:${UsbBasicKeyboardActionCounter} | `);
+    process.stdout.write(`UsbMedia:${UsbMediaKeyboardActionCounter} | `);
+    process.stdout.write(`UsbSystem:${UsbSystemKeyboardActionCounter} | `);
+    process.stdout.write(`UsbMouse:${UsbMouseActionCounter}`);
     process.stdout.write('\n');
 
-    process.stdout.write(`generic:${UsbGenericHidActionCounter - prevGeneric} `)
-    process.stdout.write(`basic:${UsbBasicKeyboardActionCounter - prevBasic} `)
-    process.stdout.write(`basic:${UsbMediaKeyboardActionCounter - prevMedia} `)
-    process.stdout.write(`basic:${UsbSystemKeyboardActionCounter - prevSystem} `)
-    process.stdout.write(`basic:${UsbMouseActionCounter - prevMouse} `)
+//    process.stdout.write(`generic:${UsbGenericHidActionCounter - prevGeneric} `)
+//    process.stdout.write(`basic:${UsbBasicKeyboardActionCounter - prevBasic} `)
+//    process.stdout.write(`basic:${UsbMediaKeyboardActionCounter - prevMedia} `)
+//    process.stdout.write(`basic:${UsbSystemKeyboardActionCounter - prevSystem} `)
+//    process.stdout.write(`basic:${UsbMouseActionCounter - prevMouse} `)
 
     prevGeneric = UsbGenericHidActionCounter;
     prevBasic = UsbBasicKeyboardActionCounter;
