9a845d8f6a5485dda004446920e5868cf750dc49
László Monda <laci@monda.hu>
2018-09-10 19:29:58 +0200

    Fix eeprom.js

--- packages/usb/eeprom.js
+++ packages/usb/eeprom.js
@@ -1,13 +1,21 @@
 #!/usr/bin/env node
 const uhk = require('./uhk');
+const device = uhk.getUhkDevice();
 
-const eepromTransferType = process.argv[2];
-const eepromTransfer = uhk.eepromTransfer[eepromTransferType];
+(async function() {
+    const operationArg = process.argv[2];
+    const operation = uhk.eepromOperations[operationArg];
+    if (operation === undefined) {
+        console.error(`Invalid operation: Gotta provide one of ${Object.keys(uhk.eepromOperations).join(', ')}`);
+        process.exit(1);
+    }
 
-if (eepromTransfer === undefined) {
-    console.error(`Gotta provide one of ${Object.keys(uhk.eepromTransfer).join(', ')}`);
-    process.exit(1);
-}
+    const bufferIdArg = process.argv[3];
+    const bufferId = uhk.configBufferIds[bufferIdArg]
+    if (bufferId === undefined) {
+        console.error(`Invalid bufferId: Gotta provide one of ${Object.keys(uhk.configBufferIds).join(', ')}`);
+        process.exit(1);
+    }
 
-//    const buffer = await uhk.writeDevice(device, [uhk.usbCommands.launchEepromTransfer, eepromTransfer.operation, eepromTransfer.configBuffer]);
-
+    const buffer = await uhk.launchEepromTransfer(device, operation, bufferId);
+})();
