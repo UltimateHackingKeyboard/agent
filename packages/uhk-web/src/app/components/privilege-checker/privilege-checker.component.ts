166834e46cd0c4d8160b494c8c8c8cf06f6ee991
László Monda <laci@monda.hu>
2018-10-08 00:22:14 +0200

    Update udev rules based on the instructions of https://github.com/node-hid/node-hid/releases/tag/v0.7.2

--- packages/uhk-web/src/app/components/privilege-checker/privilege-checker.component.ts
+++ packages/uhk-web/src/app/components/privilege-checker/privilege-checker.component.ts
@@ -22,7 +22,9 @@ export class PrivilegeCheckerComponent implements OnInit, OnDestroy {
 # Ultimate Hacking Keyboard rules
 # These are the udev rules for accessing the USB interfaces of the UHK as non-root users.
 # Copy this file to /etc/udev/rules.d and physically reconnect the UHK afterwards.
-SUBSYSTEMS=="usb", ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="612[0-7]", MODE:="0666"
+SUBSYSTEM=="input", GROUP="input", MODE="0666"
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="612[0-7]", MODE:="0666", GROUP="plugdev"
+KERNEL=="hidraw*", ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="612[0-7]", MODE="0666", GROUP="plugdev"
 EOF
 udevadm trigger
 udevadm settle`;
