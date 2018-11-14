a52b34fc3fabc12a3a513d2ab29bddb1df6aa905
Róbert Kiss <erT78gb@gmail.com>
2018-11-14 23:14:05 +0100

    Revert "chore: rebuild node-hid to hide USB usage data (#853)" (#861)
    This reverts commit 1a14ac020e76ecf5f13fd333a3ff6270f94bc600.

--- scripts/rebuild-node-hid.js
+++
@@ -1,6 +0,0 @@
-const {execSync} = require('child_process');
-const {join} = require('path');
-
-const cwd = join(__dirname, '../packages/uhk-agent/dist/node_modules/node-hid');
-console.log(`rebuild node-hid in ${cwd}`);
-execSync('node-gyp rebuild', {cwd, shell: true, stdio: [0, 1, 2]});
