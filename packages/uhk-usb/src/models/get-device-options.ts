7bfb59232562dd4f7097835ec4a9be51ab7cfd42
Róbert Kiss <erT78gb@gmail.com>
2019-11-04 11:34:24 +0100

    fix: log entries colors when upgrading firmware. (#1086)

---
+++ packages/uhk-usb/src/models/get-device-options.ts
@@ -0,0 +1,7 @@
+export interface GetDeviceOptions {
+    /**
+     * Set the log level of error messages when try to connecting to the keyboard
+     * Default value: error
+     */
+    errorLogLevel?: string;
+}
---
+++ packages/uhk-usb/src/models/index.ts
@@ -0,0 +1,2 @@
+export * from './get-device-options';
+export * from './load-configurations-result';
