1a341985363f4978e77bd02fecc923dfd5d70a67
Róbert Kiss <ert78gb@gmail.com>
2019-11-13 20:50:19 +0100

    feat: arrow animation

--- packages/uhk-web/src/app/components/highlight-arrow/highlight-arrow.component.ts
+++ packages/uhk-web/src/app/components/highlight-arrow/highlight-arrow.component.ts
@@ -1,20 +1,10 @@
-import { animate, trigger, transition, style } from '@angular/animations';
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
     selector: 'highlight-arrow',
     templateUrl: './highlight-arrow.component.html',
     styleUrls: ['./highlight-arrow.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    animations: [
-        trigger('highlightProgressButton', [
-            transition(':enter', [
-                style({ transform: 'translateY(-50rem) scale(4)', opacity: 0 }),
-                animate('500ms ease-in', style({ transform: 'translateY(-25rem) scale(2)', opacity: 1 })),
-                animate('1000ms ease-out', style({ transform: 'translateY(0) scale(1)', opacity: 0 }))
-            ])
-        ])
-    ]
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HighlightArrowComponent {
 }
