655466d36b18da1f469ac784a902ac2556e0df8b
László Monda <laci@monda.hu>
2019-12-08 23:15:59 +0100

    Display left and right modules.

--- packages/usb/get-device-state.js
+++ packages/usb/get-device-state.js
@@ -2,6 +2,22 @@
 const uhk = require('./uhk');
 const device = uhk.getUhkDevice();
 
+const moduleIdToString = [
+    'UnknownModule',
+    'LeftKeyboardHalf',
+    'KeyClusterLeft',
+    'TrackballRight',
+    'TrackpointRight',
+    'TouchpadRight',
+];
+
+const layerNumberToString = [
+    'base',
+    'mod',
+    'fn',
+    'mouse',
+];
+
 function readKeyboardState() {
     const payload = Buffer.from([uhk.usbCommands.getDeviceState]);
     console.log('Sending ', uhk.bufferToString(payload));
@@ -10,10 +26,16 @@ function readKeyboardState() {
     console.log('Received', uhk.bufferToString(receivedBuffer));
     const isEepromBusy = receivedBuffer[1] !== 0 ? 'yes' : 'no ';
     const areHalvesMerged = receivedBuffer[2] !== 0 ? 'yes' : 'no ';
-    const isLeftHalfConnected = receivedBuffer[3] !== 0 ? 'yes' : 'no ';
     const activeLayerNumber = receivedBuffer[6];
-    const activelayerName = ['base', 'mod', 'fn', 'mouse'][activeLayerNumber];
-    console.log(`isEepromBusy: ${isEepromBusy} | areHalvesMerged: ${areHalvesMerged} | isLeftHalfConnected:${isLeftHalfConnected} | ${activelayerName} layer`);
+    const activelayerName = layerNumberToString[activeLayerNumber];
+    console.log(
+`isEepromBusy: ${isEepromBusy} | \
+areHalvesMerged: ${areHalvesMerged} | \
+leftKeyboardHalfSlot:${moduleIdToString[receivedBuffer[3]]} | \
+leftModuleSlot:${moduleIdToString[receivedBuffer[4]]} | \
+rightModuleSlot:${moduleIdToString[receivedBuffer[5]]} | \
+layer:${activelayerName}`
+    );
     setTimeout(readKeyboardState, 500)
 }
 
