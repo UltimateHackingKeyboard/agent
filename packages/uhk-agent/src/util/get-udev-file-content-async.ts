80851864b4b8767055d999eb94995ea365280e9e
Róbert Kiss <ert78gb@gmail.com>
2020-01-21 21:06:35 +0100

    fix: udev file content reading on non Linux platform

--- packages/uhk-agent/src/util/get-udev-file-content-async.ts
+++ packages/uhk-agent/src/util/get-udev-file-content-async.ts
@@ -5,6 +5,10 @@ import { promisify } from 'util';
 const readFileAsync = promisify(readFile);
 
 export async function getUdevFileContentAsync(rootDir: string): Promise<string> {
+    if (process.platform !== 'linux') {
+        return '';
+    }
+
     const filePath = join(rootDir, 'rules/50-uhk60.rules');
 
     return readFileAsync(filePath, { encoding: 'utf-8' });
