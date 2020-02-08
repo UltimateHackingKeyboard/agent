07e0786a9fa08df47103825771999c3069fb2669
Róbert Kiss <erT78gb@gmail.com>
2020-02-08 13:52:00 +0100

    feat: '--preserve-udev-rules' command line argument (#1162)

--- packages/uhk-common/src/models/command-line-args.ts
+++ packages/uhk-common/src/models/command-line-args.ts
@@ -4,6 +4,14 @@ export interface CommandLineArgs {
      */
     addons?: boolean;
     /**
+     * show help.
+     */
+    help?: boolean;
+    /**
+     * Agent not force the udev rule update
+     */
+    'preserve-udev-rules'?: boolean;
+    /**
      * simulate privilege escalation error
      */
     spe?: boolean;
