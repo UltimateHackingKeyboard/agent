08067781f32cc3b2f75dee810e6987113018e90c
Róbert Kiss <ert78gb@gmail.com>
2020-05-17 19:19:18 +0200

    ci: release with GitHub Actions

--- scripts/release.js
+++ scripts/release.js
@@ -39,9 +39,16 @@ if (process.env.TRAVIS) {
 
 console.log({ branchName, pullRequestNr, gitTag, repoName, githubRef, githubEventName });
 
-const isReleaseCommit = TEST_BUILD || branchName === gitTag && repoName === 'UltimateHackingKeyboard/agent';
+const isReleaseCommit = TEST_BUILD
+    || process.env.GITHUB_ACTIONS
+    || branchName === gitTag && repoName === 'UltimateHackingKeyboard/agent';
 
-if (!isReleaseCommit && !process.env.GITHUB_ACTIONS) {
+if (process.env.GITHUB_ACTIONS && githubEventName !== 'release') {
+    console.log(`"${githubEventName}" if not release GitHub Event. Skipping publish.`);
+    process.exit(0)
+}
+
+if (!isReleaseCommit) {
     console.log('It is not a release task. Skipping publish.');
     process.exit(0)
 }
