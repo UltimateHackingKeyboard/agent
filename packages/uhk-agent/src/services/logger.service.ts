5b624319c1020a11775e77f96eb8b0de4257f9c5
Róbert Kiss <erT78gb@gmail.com>
2020-05-28 17:32:50 +0200

    chore: upgrade electron-log => 4.2.0 (#1260)

--- packages/uhk-agent/src/services/logger.service.ts
+++ packages/uhk-agent/src/services/logger.service.ts
@@ -1,6 +1,10 @@
+import * as path from 'path';
 import * as log from 'electron-log';
-log.transports.file.level = 'debug';
+
 log.transports.console.level = 'debug';
-log.transports.rendererConsole.level = 'debug';
+log.transports.file.level = 'debug';
+log.transports.file.resolvePath = variables => {
+    return path.join(variables.libraryDefaultDir, 'uhk-agent.log');
+};
 
 export const logger = log;
