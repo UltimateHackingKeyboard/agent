c3424ecb1c0bd6ee1258dc37246743c894ff8c19
Róbert Kiss <ert78gb@gmail.com>
2020-09-12 22:08:11 +0200

    chore: build command

--- packages/usb/kboot-firmware-upgrade.ts
+++ packages/usb/kboot-firmware-upgrade.ts
@@ -2,25 +2,25 @@ import * as path from 'path';
 import { LogService } from 'uhk-common';
 import { UhkHidDevice, UhkOperations } from 'uhk-usb';
 
-const logService = new LogService();
-const rootDir = path.join(__dirname, '../../tmp');
-const uhkHidDevice = new UhkHidDevice(logService, {}, rootDir);
-const uhkOperations = new UhkOperations(logService, uhkHidDevice, rootDir);
-
-process.on('uncaughtException', error => {
-    console.error('uncaughtException',  error);
-    process.exit(1);
-});
-
-process.on('unhandledRejection', (reason: any, promise: Promise<any>): void => {
-    console.error('unhandledRejection', { reason, promise });
-});
-
-uhkOperations
-    .updateRightFirmwareWithKboot()
-    .then(() => uhkOperations.updateLeftModuleWithKboot())
-    .then(() => console.log('Firmware upgrade finished'))
-    .catch(error => {
-        console.error(error);
-        process.exit(-1);
-    });
+// const logService = new LogService();
+// const rootDir = path.join(__dirname, '../../tmp');
+// const uhkHidDevice = new UhkHidDevice(logService, {}, rootDir);
+// const uhkOperations = new UhkOperations(logService, uhkHidDevice, rootDir);
+//
+// process.on('uncaughtException', error => {
+//     console.error('uncaughtException',  error);
+//     process.exit(1);
+// });
+//
+// process.on('unhandledRejection', (reason: any, promise: Promise<any>): void => {
+//     console.error('unhandledRejection', { reason, promise });
+// });
+//
+// uhkOperations
+//     .updateRightFirmwareWithKboot()
+//     .then(() => uhkOperations.updateLeftModuleWithKboot())
+//     .then(() => console.log('Firmware upgrade finished'))
+//     .catch(error => {
+//         console.error(error);
+//         process.exit(-1);
+//     });
