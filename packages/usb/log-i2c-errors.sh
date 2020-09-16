9f4c9bdc302e6c7921922acb4859f24eba81a71d
Róbert Kiss <ert78gb@gmail.com>
2020-09-16 22:05:10 +0200

    log-i2c-errors.sh

--- packages/usb/log-i2c-errors.sh
+++ packages/usb/log-i2c-errors.sh
@@ -5,6 +5,6 @@ startTime=`date +%s`
 while true; do
     s="$((`date +%s`-$startTime))"
     echo "$((s/60))m"
-    for i in `seq 0 5`; do ./get-slave-i2c-errors.js $i; done
+    for i in `seq 0 5`; do ./get-slave-i2c-errors.ts $i; done
 done
 ) 2>&1 | tee $1
