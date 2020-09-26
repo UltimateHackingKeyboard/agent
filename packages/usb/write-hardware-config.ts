f77f123d7d9279e49c99fa36dd0057fb7b819977
Róbert Kiss <ert78gb@gmail.com>
2020-09-26 20:11:23 +0200

    write-hardware-config.ts

---
+++ packages/usb/write-hardware-config.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env ts-node-script
+
+import Uhk, { errorHandler, yargs } from './src';
+
+(async () => {
+    try {
+        const argv = yargs
+            .scriptName('./switch-keymap.ts')
+            .usage('Usage: $0 {iso|ansi}')
+            .demandCommand(1, 'Layout is required.')
+            .argv;
+
+        const layout = argv._[0];
+
+        if (!['ansi', 'iso'].includes(layout)) {
+            console.log('Invalid layout. Layout should be either iso or ansi');
+            process.exit(1);
+        }
+        const { operations } = Uhk(argv);
+        await operations.saveHardwareConfiguration(layout === 'iso');
+
+    } catch (error) {
+        errorHandler(error);
+    }
+})();
