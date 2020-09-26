7eebdaa586a375269d230e5a141625bf307ecae7
Róbert Kiss <ert78gb@gmail.com>
2020-09-26 20:06:54 +0200

    wait-for-kboot-idle.ts

---
+++ packages/usb/wait-for-kboot-idle.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env ts-node-script
+
+import Uhk, { errorHandler, yargs } from './src';
+
+(async () => {
+    try {
+        const argv = yargs
+            .scriptName('./switch-keymap.ts')
+            .usage('Usage: $0')
+            .argv as any;
+
+        const { operations } = Uhk(argv);
+        await operations.waitForKbootIdle();
+
+    } catch (error) {
+        errorHandler(error);
+    }
+})();
