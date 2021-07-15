59bfb9e823f79db2af94d472ecbb7947ad21df25
Róbert Kiss <ert78gb@gmail.com>
2021-07-15 18:31:06 +0200

    chore: upgrade angular => 12.1.2

--- packages/uhk-web/src/polyfills.ts
+++ packages/uhk-web/src/polyfills.ts
@@ -18,22 +18,6 @@
  * BROWSER POLYFILLS
  */
 
-/** IE9, IE10 and IE11 requires all of the following polyfills. **/
-import 'core-js/es/symbol';
-import 'core-js/es/object';
-import 'core-js/es/function';
-import 'core-js/es/parse-int';
-import 'core-js/es/parse-float';
-import 'core-js/es/number';
-import 'core-js/es/math';
-import 'core-js/es/string';
-import 'core-js/es/date';
-import 'core-js/es/array';
-import 'core-js/es/regexp';
-import 'core-js/es/map';
-import 'core-js/es/weak-map';
-import 'core-js/es/set';
-
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 import 'classlist.js';  // Run `npm install --save classlist.js`.
 
--- packages/uhk-web/webpack.config.js
+++ packages/uhk-web/webpack.config.js
@@ -1,14 +1,6 @@
 module.exports = {
     "target": "electron-renderer",
     "node": {
-        "fs": true,
         "global": true,
-        "crypto": "empty",
-        "tls": "empty",
-        "net": "empty",
-        "process": true,
-        "module": false,
-        "clearImmediate": false,
-        "setImmediate": false
     }
 };
