bed15c5a5bb70f5dff0e6e3460b0cd92bab0d9d5
Kiss Róbert <ert78gb@gmail.com>
2021-12-21 09:38:05 +0100

    chore: remove classlist.js it was required for IE11

--- packages/uhk-web/src/polyfills.ts
+++ packages/uhk-web/src/polyfills.ts
@@ -18,20 +18,11 @@
  * BROWSER POLYFILLS
  */
 
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-import 'classlist.js';  // Run `npm install --save classlist.js`.
-
 /** Evergreen browsers require these. **/
 import 'core-js/es/reflect';
 import 'core-js/es/array';
 import 'core-js/es/object';
 import 'core-js/es/reflect';
-
-/**
- * Required to support Web Animations `@angular/animation`.
- * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
- **/
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
