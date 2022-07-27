a1f47b43c269424cc4e7e54718e0691c9c2657f9
László Monda <laci@monda.hu>
2022-07-27 12:23:00 +0200

    Update smart macro documentation.

--- smart-macro-docs/script.mjs
+++ smart-macro-docs/script.mjs
@@ -1,4 +1,4 @@
-import {createApp} from './node_modules/vue/dist/vue.esm-browser.prod.js';
+import {createApp} from './vue.esm-browser.prod.js';
 
 // Components
 
@@ -13,6 +13,9 @@ function updateWidgets() {
 }
 
 function getVariable(name) {
+    if (!name) {
+        return undefined;
+    }
     const regexVarName = name.replace(/\\./g, '\\.');
     const regex = new RegExp(`set +${regexVarName} +(\\S+) *#?`);
     const matches = currentCommand.match(regex);
@@ -21,12 +24,15 @@ function getVariable(name) {
 }
 
 function setVariable(name, value) {
+    if (!name) {
+        return;
+    }
     const regexVarName = name.replace(/\\./g, '\\.');
     const regex = new RegExp(`(set +${regexVarName} +)\\S+( *#?)`);
     if (regex.test(currentCommand)) {
         currentCommand = currentCommand.replace(regex, `$1${value}$2`);
     } else {
-        currentCommand += (currentCommand.endsWith('\n') ? '' : '\n') + `set ${regexVarName} ${value}\n`;
+        currentCommand += (currentCommand.endsWith('\n') || currentCommand.trim() === '' ? '' : '\n') + `set ${regexVarName} ${value}\n`;
     }
     const message = {
         action: 'doc-message-set-macro',
@@ -37,10 +43,14 @@ function setVariable(name, value) {
 }
 
 const Checkbox = {
-    template: `<input type="checkbox" ref="input" @input="updateValue">`,
+    template: `<input type="checkbox" ref="input" @change="updateValue" v-model="checked">`,
+    emits: [
+        'update:modelValue',
+    ],
     props: {
         name: String,
         default: Number,
+        modelValue: String,
     },
     data() {
         return {
@@ -53,27 +63,32 @@ const Checkbox = {
     },
     methods: {
         initWidget() {
-            const value = getVariable(this.name) ?? this.default;
-            this.value = this.$refs.input.value = value;
+            this.checked = getVariable(this.name) ?? this.default === '1';
         },
         updateValue(isInit) {
             if (isInit === true) {
-                this.$refs.input.checked = this.default === '1' ? 'checked' : 0;
+                this.checked = this.default === '1';
             }
-            this.value = +this.$refs.input.checked ? 1 : 0;
-            if (isInit !== true) {
-                setVariable(this.name, this.value, isInit);
+
+            if (isInit !== true && this.name) {
+                setVariable(this.name, this.checked ? '0' : '1', isInit);
             }
+
+            this.$emit('update:modelValue', this.checked);
         },
     },
 };
 
 const Dropdown = {
     template: `<select ref="input" @input="updateValue" class="form-select"><option v-for="option in selectOptions" :value="option.value">{{option.label}}</option></select>`,
+    emits: [
+        'update:modelValue',
+    ],
     props: {
         name: String,
-        options: String,
+        options: Array,
         default: String,
+        modelValue: String,
     },
     data() {
         return {
@@ -81,7 +96,7 @@ const Dropdown = {
             selectOptions: [],
         };
     },
-    async mounted() {
+    mounted() {
         const allOptions = {
             modifierTriggers: [
                 {label:'Both', value:'both'},
@@ -98,9 +113,27 @@ const Dropdown = {
                 {label:'Zoom Mac', value:'zoomMac'},
                 {label:'None', value:'none'},
             ],
+            touchpadNavigationModes: [
+                {label:'Zoom', value:'zoom'},
+                {label:'Zoom PC', value:'zoomPc'},
+                {label:'Zoom Mac', value:'zoomMac'},
+                {label:'Media', value:'media'},
+                {label:'None', value:'none'},
+            ],
+            navigationModeActionModes: [
+                {label:'Caret', value:'caret'},
+                {label:'Media', value:'media'},
+                {label:'Zoom PC', value:'zoomPc'},
+                {label:'Zoom Mac', value:'zoomMac'},
+            ],
+            directions: [
+                {label:'Up', value:'up'},
+                {label:'Down', value:'down'},
+                {label:'Left', value:'left'},
+                {label:'Right', value:'right'},
+            ],
         }
         this.selectOptions = allOptions[this.options];
-        await this.$nextTick();
         this.updateValue(true);
         widgetDict[this.name] = this;
     },
@@ -112,11 +145,13 @@ const Dropdown = {
         updateValue(isInit) {
             if (isInit === true) {
                 this.$refs.input.value = this.default;
+                this.$emit('update:modelValue', this.default);
             }
 
             this.value = this.$refs.input.value;
             if (isInit !== true) {
                 setVariable(this.name, this.value, isInit);
+                this.$emit('update:modelValue', this.value);
             }
         },
     },
@@ -155,6 +190,37 @@ const Slider = {
             }
         },
     },
+};
+
+const Select2 = {
+    props: {
+        options: Object,
+        modelValue: String,
+    },
+    emits: [
+        'update:modelValue',
+    ],
+    template: '<select><slot></slot></select>',
+    mounted() {
+        const vm = this;
+        $(this.$el)
+            .select2({data: this.options})
+            .val(this.value)
+            .on('change', function() {
+                vm.$emit('update:modelValue', this.value);
+            });
+    },
+    watch: {
+        value(value) {
+            $(this.$el).val(value).trigger('change');
+        },
+        options(options) {
+            $(this.$el).empty().select2({data: options});
+        }
+    },
+    destroyed() {
+        $(this.$el).off().select2('destroy');
+    }
 };
 
 // Init Vue
@@ -269,7 +335,237 @@ const app = createApp({
                     },
                 },
             ],
-        };
+            navigationModeActionMode: '',
+            navigationModeActionDirection: '',
+            lshift: '0',
+            lctrl: '0',
+            lalt: '0',
+            lsuper: '0',
+            rshift: '0',
+            rctrl: '0',
+            ralt: '0',
+            rsuper: '0',
+            scancode: '(none)',
+            scancodes: [
+                '(none)',
+                'enter',
+                'escape',
+                'backspace',
+                'tab',
+                'space',
+                'minusAndUnderscore',
+                'equalAndPlus',
+                'openingBracketAndOpeningBrace',
+                'closingBracketAndClosingBrace',
+                'backslashAndPipeIso',
+                'backslashAndPipe',
+                'nonUsHashmarkAndTilde',
+                'semicolonAndColon',
+                'apostropheAndQuote',
+                'graveAccentAndTilde',
+                'commaAndLessThanSign',
+                'dotAndGreaterThanSign',
+                'slashAndQuestionMark',
+                'capsLock',
+                'printScreen',
+                'scrollLock',
+                'pause',
+                'insert',
+                'home',
+                'pageUp',
+                'delete',
+                'end',
+                'pageDown',
+                'numLock',
+                'nonUsBackslashAndPipe',
+                'application',
+                'power',
+                'keypadEqualSign',
+                'execute',
+                'help',
+                'menu',
+                'select',
+                'stop',
+                'again',
+                'undo',
+                'cut',
+                'copy',
+                'paste',
+                'find',
+                'mute',
+                'volumeUp',
+                'volumeDown',
+                'lockingCapsLock',
+                'lockingNumLock',
+                'lockingScrollLock',
+                'keypadComma',
+                'keypadEqualSignAs400',
+                'international1',
+                'international2',
+                'international3',
+                'international4',
+                'international5',
+                'international6',
+                'international7',
+                'international8',
+                'international9',
+                'lang1',
+                'lang2',
+                'lang3',
+                'lang4',
+                'lang5',
+                'lang6',
+                'lang7',
+                'lang8',
+                'lang9',
+                'alternateErase',
+                'sysreq',
+                'cancel',
+                'clear',
+                'prior',
+                'return',
+                'separator',
+                'out',
+                'oper',
+                'clearAndAgain',
+                'crselAndProps',
+                'exsel',
+                'keypad00',
+                'keypad000',
+                'thousandsSeparator',
+                'decimalSeparator',
+                'currencyUnit',
+                'currencySubUnit',
+                'keypadOpeningParenthesis',
+                'keypadClosingParenthesis',
+                'keypadOpeningBrace',
+                'keypadClosingBrace',
+                'keypadTab',
+                'keypadBackspace',
+                'keypadA',
+                'keypadB',
+                'keypadC',
+                'keypadD',
+                'keypadE',
+                'keypadF',
+                'keypadXor',
+                'keypadCaret',
+                'keypadPercentage',
+                'keypadLessThanSign',
+                'keypadGreaterThanSign',
+                'keypadAmp',
+                'keypadAmpAmp',
+                'keypadPipe',
+                'keypadPipePipe',
+                'keypadColon',
+                'keypadHashmark',
+                'keypadSpace',
+                'keypadAt',
+                'keypadExclamationSign',
+                'keypadMemoryStore',
+                'keypadMemoryRecall',
+                'keypadMemoryClear',
+                'keypadMemoryAdd',
+                'keypadMemorySubtract',
+                'keypadMemoryMultiply',
+                'keypadMemoryDivide',
+                'keypadPlusAndMinus',
+                'keypadClear',
+                'keypadClearEntry',
+                'keypadBinary',
+                'keypadOctal',
+                'keypadDecimal',
+                'keypadHexadecimal',
+                'keypadSlash',
+                'keypadAsterisk',
+                'keypadMinus',
+                'keypadPlus',
+                'keypadEnter',
+                'keypad1AndEnd',
+                'keypad2AndDownArrow',
+                'keypad3AndPageDown',
+                'keypad4AndLeftArrow',
+                'keypad5',
+                'keypad6AndRightArrow',
+                'keypad7AndHome',
+                'keypad8AndUpArrow',
+                'keypad9AndPageUp',
+                'keypad0AndInsert',
+                'keypadDotAndDelete',
+                'leftControl',
+                'leftShift',
+                'leftAlt',
+                'leftGui',
+                'rightControl',
+                'rightShift',
+                'rightAlt',
+                'rightGui',
+                'up',
+                'down',
+                'left',
+                'right',
+                'upArrow',
+                'downArrow',
+                'leftArrow',
+                'rightArrow',
+                'np0',
+                'np1',
+                'np2',
+                'np3',
+                'np4',
+                'np5',
+                'np6',
+                'np7',
+                'np8',
+                'np9',
+                'f1',
+                'f2',
+                'f3',
+                'f4',
+                'f5',
+                'f6',
+                'f7',
+                'f8',
+                'f9',
+                'f10',
+                'f11',
+                'f12',
+                'f13',
+                'f14',
+                'f15',
+                'f16',
+                'f17',
+                'f18',
+                'f19',
+                'f20',
+                'f21',
+                'f22',
+                'f23',
+                'f24',
+                'mediaVolumeMute',
+                'mediaVolumeUp',
+                'mediaVolumeDown',
+                'mediaRecord',
+                'mediaFastForward',
+                'mediaRewind',
+                'mediaNext',
+                'mediaPrevious',
+                'mediaStop',
+                'mediaPlayPause',
+                'mediaPause',
+                'systemPowerDown',
+                'systemSleep',
+                'systemWakeUp',
+                'mouseBtnLeft',
+                'mouseBtnRight',
+                'mouseBtnMiddle',
+                'mouseBtn4',
+                'mouseBtn5',
+                'mouseBtn6',
+                'mouseBtn7',
+                'mouseBtn8',
+            ],
+        }
     },
     created() {
         const self = this;
@@ -292,7 +588,13 @@ const app = createApp({
                 }
             }
         });
+
+        // Open select2 upon clicking on it.
         window.parent.postMessage({action: 'doc-message-inited'}, '*');
+        $(document).on('select2:open', () => {
+            document.querySelector('.select2-search__field').focus();
+        });
+
     },
     methods: {
         getNavigationMode(module, layer) {
@@ -301,16 +603,37 @@ const app = createApp({
             }
             return {Base:'cursor', Mod:'scroll', Fn:'caret'}[layer] ?? 'cursor';
         },
+        getScancode() {
+            const vm = this;
+            const modifierNameToMask = [
+                {name:'lshift', mask:'LS'},
+                {name:'lctrl', mask:'LC'},
+                {name:'lalt', mask:'LA'},
+                {name:'lsuper', mask:'LG'},
+                {name:'rshift', mask:'RS'},
+                {name:'rctrl', mask:'RC'},
+                {name:'ralt', mask:'RA'},
+                {name:'rsuper', mask:'RG'},
+            ];
+            const modifierMask = modifierNameToMask.filter(modifier => vm[modifier.name]).map(modifier => modifier.mask).join('');
+            const separator = modifierMask && this.scancode !== '(none)' ? '-' : '';
+            const scancode = this.scancode === '(none)' ? '' : this.scancode;
+            return `${modifierMask}${separator}${scancode}`;
+        }
     },
     computed: {
         rightModules() {
             return this.modules.filter(module => module !== 2);
+        },
+        isTouchpadAttached() {
+            return this.modules.includes(5);
         }
-    }
+    },
 });
 
 app.component('Checkbox', Checkbox);
 app.component('Dropdown', Dropdown);
 app.component('Slider', Slider);
+app.component('Select2', Select2);
 
 app.mount('body');
