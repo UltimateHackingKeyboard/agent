e19cb4554dc48a36f093e4651d2d8842d13f02f5
Nick Cao <nickcao@nichi.co>
2023-02-27 13:48:34 +0800

    chore(web): resolve path to node_modules with webpack alias

--- packages/uhk-web/webpack.config.js
+++ packages/uhk-web/webpack.config.js
@@ -2,5 +2,11 @@ module.exports = {
     "target": "electron-renderer",
     "node": {
         "global": true,
+    },
+    "resolve": {
+        "alias": {
+            "monaco-editor": "monaco-editor",
+            "nouislider": "nouislider",
+        }
     }
 };
