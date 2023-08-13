8b1e6ba5a16738ff8aa3a58a28472a0eb0a865db
Kiss Róbert <ert78gb@gmail.com>
2023-08-13 20:13:05 +0200

    feat: add color to palette via drag-and-drop

--- packages/uhk-web/src/app/pipes/index.ts
+++ packages/uhk-web/src/app/pipes/index.ts
@@ -1,3 +1,4 @@
+export  { AsHexColorPipe } from './as-hex-color.pipe';
 export  { SafeStylePipe } from './safe-style.pipe';
 export  { SafeUrlPipe } from './safe-url.pipe';
 export { UserConfigHistoryDisplayTextPipe } from './user-config-history-display-text.pipe';
