a47317984d4f46ae2022dd7b33e9e243284fab76
Kiss Róbert <ert78gb@gmail.com>
2023-09-09 18:22:24 +0200

    feat: send-command.ts

--- packages/usb/send-command.ts
+++ packages/usb/send-command.ts
@@ -1,12 +1,36 @@
 #!/usr/bin/env -S node --loader ts-node/esm --no-warnings=ExperimentalWarning
 
-import Uhk, { errorHandler, getI2cAddressFromArg, yargs } from './src/index.js';
+import Uhk, { errorHandler, yargs } from './src/index.js';
+import { Buffer } from 'uhk-common';
 
 const argv = yargs
     .scriptName('./send-command.ts')
     .usage('Send USB command to the keyboard')
     .argv;
 
-const { device } = Uhk(argv);
+try {
+    if (argv._.length === 0) {
+        console.error('No command arguments specified');
+        process.exit(1);
+    }
 
-await device.write(argv._.map(arg => parseInt(arg, 16))
+    const { device } = Uhk(argv);
+
+    const buffer = Buffer.from(argv._.map(arg => parseInt(arg, 10)));
+
+    const response = await device.write(buffer);
+
+    process.stdout.write('Byte result:\n');
+    for(const byte of response) {
+        process.stdout.write(byte.toString(10));
+        process.stdout.write(' ');
+    }
+
+    process.stdout.write('\n');
+    process.stdout.write('\n');
+    process.stdout.write('String result:\n');
+    process.stdout.write(response.toString('utf8', 1));
+    process.stdout.write('\n');
+} catch (error) {
+    errorHandler(error);
+}
