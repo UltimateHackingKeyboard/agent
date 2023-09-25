2bde25b583c9232cc7ba8d095efeb6093604756f
Kiss Róbert <ert78gb@gmail.com>
2023-09-25 10:48:13 +0200

    fix: normalization

--- packages/uhk-usb/src/utils/normalize-status-buffer.ts
+++ packages/uhk-usb/src/utils/normalize-status-buffer.ts
@@ -2,12 +2,17 @@ export function normalizeStatusBuffer(message: string): string {
     const errors = new Set<string>();
     let error = '';
 
+    function addToSet(error:string) {
+        const trimmed = error.trim();
+        if (trimmed) {
+            errors.add(trimmed);
+        }
+    }
+
     const lines = message.split('\n');
     for (const line of lines) {
         if (line.startsWith('Error at') || line.startsWith('Warning at')) {
-            if (error) {
-                errors.add(error);
-            }
+            addToSet(error);
 
             error = line;
         } else {
@@ -15,7 +20,7 @@ export function normalizeStatusBuffer(message: string): string {
         }
     }
 
-    errors.add(error);
+    addToSet(error);
 
     return Array.from(errors).join('\n');
 }
