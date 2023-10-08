efbbf94c5184a2224ebb77273e6f1e750a9b285e
Róbert Kiss <erT78gb@gmail.com>
2023-10-08 18:48:45 +0200

    fix: suggestion widget visibility, bump monaco-editor 0.41.0 => 0.44.0 (#2104)

--- packages/uhk-web/src/app/components/macro/action-editor/tab/command/macro-command-editor.component.ts
+++ packages/uhk-web/src/app/components/macro/action-editor/tab/command/macro-command-editor.component.ts
@@ -23,7 +23,7 @@ import { SelectedMacroActionId } from '../../../../../models';
 import { SmartMacroDocCommandAction, SmartMacroDocService } from '../../../../../services/smart-macro-doc-service';
 import { hasNonAsciiCharacters, NON_ASCII_REGEXP } from '../../../../../util';
 
-const MONACO_EDITOR_LINE_HEIGHT_OPTION = 65;
+const MONACO_EDITOR_LINE_HEIGHT_OPTION = 66;
 const MONACO_EDITOR_LF_END_OF_LINE_OPTION = 0;
 const MACRO_CHANGE_DEBOUNCE_TIME = 250;
 
