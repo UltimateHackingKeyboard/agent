75978c1e4a77b0928895d93f43f1c3c24986fe2d
Kiss Róbert <ert78gb@gmail.com>
2024-01-27 20:05:45 +0100

    fix: navigation mode options

--- packages/uhk-web/src/app/components/add-on/add-on.component.ts
+++ packages/uhk-web/src/app/components/add-on/add-on.component.ts
@@ -39,6 +39,33 @@ export class AddOnComponent {
     // Touchpad
     pitchToZoomOptions = [
         {
+            value: 'zoom',
+            label: 'Zoom'
+        },
+        {
+            value: 'zoomPc',
+            label: 'Zoom PC'
+        },
+        {
+            value: 'zoomMac',
+            label: 'Zoom Mac'
+        },
+        {
+            value: 'none',
+            label: 'None'
+        }
+    ];
+    touchpadPinchToZoom: 'zoom' | 'zoomPc' | 'zoomMac' | 'none' = 'zoom';
+    touchpadPinchZoomDivisor= 4;
+    touchpadHoldContinuationTimeout= 4;
+
+    // navigation modes
+    navigationModOptions = [
+        {
+            value: 'cursor',
+            label: 'Cursor'
+        },
+        {
             value: 'media',
             label: 'Media'
         },
@@ -63,19 +90,15 @@ export class AddOnComponent {
             label: 'None'
         }
     ];
-    touchpadPinchToZoom: 'media' | 'caret' | 'zoom' | 'zoomPc' | 'zoomMac' | 'none' = 'zoom';
-    touchpadPinchZoomDivisor= 4;
-    touchpadHoldContinuationTimeout= 4;
 
-    // navigation modes
-    navigationModeBase: 'media' | 'caret' | 'zoom' | 'zoomPc' | 'zoomMac' | 'none' = 'zoom';
-    navigationModeMod: 'media' | 'caret' | 'zoom' | 'zoomPc' | 'zoomMac' | 'none' = 'zoom';
-    navigationModeMouse: 'media' | 'caret' | 'zoom' | 'zoomPc' | 'zoomMac' | 'none' = 'zoom';
-    navigationModeFn: 'media' | 'caret' | 'zoom' | 'zoomPc' | 'zoomMac' | 'none' = 'zoom';
-    navigationModeFn2: 'media' | 'caret' | 'zoom' | 'zoomPc' | 'zoomMac' | 'none' = 'zoom';
-    navigationModeFn3: 'media' | 'caret' | 'zoom' | 'zoomPc' | 'zoomMac' | 'none' = 'zoom';
-    navigationModeFn4: 'media' | 'caret' | 'zoom' | 'zoomPc' | 'zoomMac' | 'none' = 'zoom';
-    navigationModeFn5: 'media' | 'caret' | 'zoom' | 'zoomPc' | 'zoomMac' | 'none' = 'zoom';
+    navigationModeBase: 'cursor' | 'media' | 'caret' | 'zoom' | 'zoomPc' | 'zoomMac' | 'none' = 'cursor';
+    navigationModeMod: 'cursor' | 'media' | 'caret' | 'zoom' | 'zoomPc' | 'zoomMac' | 'none' = 'cursor';
+    navigationModeMouse: 'cursor' | 'media' | 'caret' | 'zoom' | 'zoomPc' | 'zoomMac' | 'none' = 'cursor';
+    navigationModeFn: 'cursor' | 'media' | 'caret' | 'zoom' | 'zoomPc' | 'zoomMac' | 'none' = 'cursor';
+    navigationModeFn2: 'cursor' | 'media' | 'caret' | 'zoom' | 'zoomPc' | 'zoomMac' | 'none' = 'cursor';
+    navigationModeFn3: 'cursor' | 'media' | 'caret' | 'zoom' | 'zoomPc' | 'zoomMac' | 'none' = 'cursor';
+    navigationModeFn4: 'cursor' | 'media' | 'caret' | 'zoom' | 'zoomPc' | 'zoomMac' | 'none' = 'cursor';
+    navigationModeFn5: 'cursor' | 'media' | 'caret' | 'zoom' | 'zoomPc' | 'zoomMac' | 'none' = 'cursor';
 
     constructor(route: ActivatedRoute) {
 
