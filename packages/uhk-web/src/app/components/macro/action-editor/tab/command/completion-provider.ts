e2151064217ddf8eeb24cb9baaab5c1b890933e1
Karel Tuček <kareltucek@centrum.cz>
2024-02-09 22:24:37 +0100

    Order completion items intelligently.

--- packages/uhk-web/src/app/components/macro/action-editor/tab/command/completion-provider.ts
+++ packages/uhk-web/src/app/components/macro/action-editor/tab/command/completion-provider.ts
@@ -7,6 +7,7 @@ export class CustomCompletionProvider implements monaco.languages.CompletionItem
 
     private parser: Parser | undefined;
     private identifierCharPattern = /[a-zA-Z]/;
+    private realignMatcher = /(.*[^ ]) *([^()]*) *$/;
 
     constructor( private logService: LogService ) {
         this.parser = undefined;
@@ -17,15 +18,58 @@ export class CustomCompletionProvider implements monaco.languages.CompletionItem
         });
     }
 
-    guessKind(t: string): monaco.languages.CompletionItemKind {
-        if (this.identifierCharPattern.test(t[0])) {
-            return monaco.languages.CompletionItemKind.Text;
-        } else if (t[0] == '<' && t[t.length-1] == '>') {
-            return monaco.languages.CompletionItemKind.Property;
+    guessKind(text: string, rule: string): [monaco.languages.CompletionItemKind, string] {
+        if (rule.slice(rule.length-7) == '_ABBREV') {
+            // key and scancode abbreviations
+            return [monaco.languages.CompletionItemKind.Struct, "5"];
+        } else if (rule == 'MODMASK') {
+            // modmask
+            return [monaco.languages.CompletionItemKind.Interface, "4"];
+        } else  if (this.identifierCharPattern.test(text[0])) {
+            // identifiers, commands
+            return [monaco.languages.CompletionItemKind.Text, "3"];
+        } else if (text[0] == '<' && text[text.length-1] == '>') {
+            // hints
+            return [monaco.languages.CompletionItemKind.Property, "1"];
+        } else if (text[1] == '<' && text[text.length-2] == '>') {
+            // comment hints
+            return [monaco.languages.CompletionItemKind.Property, "1"];
         } else {
-            return monaco.languages.CompletionItemKind.Module;
-
+            // operators
+            return [monaco.languages.CompletionItemKind.Module, "2"];
         }
+    }
+
+    generateIconPreview(
+        model: monaco.editor.ITextModel,
+        position: monaco.Position,
+    ): monaco.languages.CompletionItem[] {
+        const lineNumber = position.lineNumber;
+        const column = position.column;
+        const lineText = model.getValueInRange({
+            startLineNumber: lineNumber,
+            startColumn: 1,
+            endLineNumber: lineNumber,
+            endColumn: column
+        });
+
+        return Array
+            .from({ length: 28 }, (_, index) => index)
+            .map(index => {
+                let item: monaco.languages.CompletionItem = {
+                    insertText: index.toString(),
+                    kind: index as monaco.languages.CompletionItemKind,
+                    label: index.toString(),
+                    range: {
+                        startLineNumber: lineNumber,
+                        startColumn: column,
+                        endLineNumber: lineNumber,
+                        endColumn: column
+                    }
+                };
+                return item;
+            }
+        );
     }
 
     provideCompletionItems(
@@ -51,20 +95,21 @@ export class CustomCompletionProvider implements monaco.languages.CompletionItem
             } catch (e) {
                 this.logService.error(e);
             }
-            let monacoSuggestions: monaco.languages.CompletionItem[] = nelaSuggestions.map (it => {
-                let kind = this.guessKind(it.suggestion);
+            let monacoSuggestions: monaco.languages.CompletionItem[] = nelaSuggestions.map(it => {
+                let kind = this.guessKind(it.suggestion, it.originRule);
                 return {
-                    label: it.suggestion,
-                    kind: kind,
-                    insertText: it.suggestion,
+                    insertText: it.text(),
+                    kind: kind[0],
+                    label: it.text(),
+                    sortText: kind[1] + it.text(),
                     range: {
                         startLineNumber: lineNumber,
                         startColumn: column - it.overlap,
                         endLineNumber: lineNumber,
                         endColumn: column
-                    }
+                    },
                 };
-            });
+            })
             return {
                 suggestions: monacoSuggestions
             };
