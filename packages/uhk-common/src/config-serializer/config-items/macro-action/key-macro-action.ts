3360569d3454957c19e7b3e0e187361bbb1989df
Kiss Róbert <ert78gb@gmail.com>
2024-02-11 16:23:39 +0100

    fix: add config v6 to key-macro-action

--- packages/uhk-common/src/config-serializer/config-items/macro-action/key-macro-action.ts
+++ packages/uhk-common/src/config-serializer/config-items/macro-action/key-macro-action.ts
@@ -73,6 +73,7 @@ export class KeyMacroAction extends MacroAction {
             case 3:
             case 4:
             case 5:
+            case 6:
             case 7:
                 this.fromBinaryV1(buffer);
                 break;
