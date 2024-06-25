0b5030d8966df7e0ba92598ee8f746f649747736
Kiss Róbert <ert78gb@gmail.com>
2024-06-25 10:21:20 +0200

    delete UserConfigHistoryDisplayTextPipe

--- packages/uhk-web/src/app/pipes/index.ts
+++ packages/uhk-web/src/app/pipes/index.ts
@@ -3,4 +3,3 @@ export  { NewLineToBrPipe } from './new-line-to-br.pipe';
 export  { SafeHtmlPipe } from './safe-html.pipe';
 export  { SafeStylePipe } from './safe-style.pipe';
 export  { SafeUrlPipe } from './safe-url.pipe';
-export { UserConfigHistoryDisplayTextPipe } from './user-config-history-display-text.pipe';
