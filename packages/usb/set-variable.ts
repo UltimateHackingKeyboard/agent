9d7402b8e8dcb2041ad1bae211bbaef8d2ac1e5b
Kiss Róbert <ert78gb@gmail.com>
2024-12-13 13:29:57 +0100

    chore: set-variable.ts supports multiple devices

--- packages/usb/set-variable.ts
+++ packages/usb/set-variable.ts
@@ -1,39 +1,53 @@
 #!/usr/bin/env -S node --loader ts-node/esm --no-warnings=ExperimentalWarning
 
-import Uhk, { errorHandler, yargs } from './src/index.js';
-import { UsbVariables } from 'uhk-usb';
+import {
+    UhkHidDevice,
+    UhkOperations,
+    UsbVariables,
+} from 'uhk-usb';
 
-(async () => {
-    try {
-        const keys = Object.keys(UsbVariables)
-            .filter((key: any) => isNaN(key))
-            .join(', ');
+import Uhk, { errorHandler, getHidDevicesFromDeviceArg, yargs } from './src/index.js';
 
-        const argv = yargs
-            .scriptName('./set-variable.ts')
-            .usage('Usage: $0 <variable> <value>')
-            .demandCommand(1, `Variable is required. Specify one of: ${keys}`)
-            .argv as any;
+try {
+    const keys = Object.keys(UsbVariables)
+        .filter((key: any) => isNaN(key))
+        .join(', ');
 
-        const variable = argv._[0] as string;
+    const argv = yargs
+        .scriptName('./set-variable.ts')
+        .usage('Usage: $0 <variable> <value>')
+        .demandCommand(2, `Variable is required. Specify one of: ${keys}`)
+        .argv as any;
 
-        if (!Object.values(UsbVariables).includes(variable)) {
-            console.error(`The specified variable does not exist. Specify one of: ${keys}`);
-            process.exit(1);
+    const variable = argv._[0] as string;
+
+    if (!Object.values(UsbVariables).includes(variable)) {
+        console.error(`The specified variable does not exist. Specify one of: ${keys}`);
+        process.exit(1);
+    }
+
+    const value = parseInt(argv._[1]);
+
+    if (isNaN(value)) {
+        console.error(`Value must be a number.`);
+        process.exit(1);
+    }
+
+    if (argv.device) {
+        const hidDevices = await getHidDevicesFromDeviceArg(argv.device);
+        const { logger, rootDir } = Uhk(argv);
+
+        for (const hidDevice of hidDevices) {
+            const uhkHidDevice = new UhkHidDevice(logger, argv, rootDir, hidDevice);
+            const uhkOperations = new UhkOperations(logger, uhkHidDevice);
+            await uhkOperations.setVariable(UsbVariables[variable], value);
         }
-
-        const value = parseInt(argv._[1]);
-
-        if (isNaN(value)) {
-            console.error(`Value must be a number.`);
-            process.exit(1);
-        }
-
+    }
+    else {
         const { operations } = Uhk(argv);
 
         await operations.setVariable(UsbVariables[variable], value);
-
-    } catch (error) {
-        await errorHandler(error);
     }
-})();
+} catch (error) {
+    await errorHandler(error);
+}
