9ee78b141e2d022ce3dd58cc6838c127dc78803b
Kiss Róbert <ert78gb@gmail.com>
2025-03-30 16:25:21 +0200

    chore: bump electron-log 4.4.8 => 5.3.3

--- packages/uhk-agent/src/services/logger.service.ts
+++ packages/uhk-agent/src/services/logger.service.ts
@@ -1,11 +1,23 @@
 import * as path from 'path';
-import * as log from 'electron-log';
+import * as log from 'electron-log/main';
 import { logUserConfigHelper, LogService, LogRegExps, UserConfiguration } from 'uhk-common';
 
+/**
+ * This service use the electron-log package to write log in file.
+ * The logger usable in main and renderer process.
+ * The location of the log files:
+ * - on Linux: ~/.config/<app name>/uhk-agent.log
+ * - on OS X: ~/Library/Logs/<app name>/uhk-agent.log
+ * - on Windows: %USERPROFILE%\AppData\Roaming\<app name>\uhk-agent.log
+ * The app name: UHK Agent. The up to date value in the scripts/release.js file.
+ */
+log.initialize({
+    spyRendererConsole: true,
+})
 log.transports.console.level = 'silly';
 log.transports.file.level = 'silly';
 log.transports.ipc.level = 'silly';
-log.transports.file.resolvePath = variables => {
+log.transports.file.resolvePathFn = variables => {
     return path.join(variables.libraryDefaultDir, 'uhk-agent.log');
 };
 
