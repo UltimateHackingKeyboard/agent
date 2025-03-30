74be9d6af9691e78fe53263540aca158433048ba
Róbert Kiss <erT78gb@gmail.com>
2025-03-30 16:34:32 +0200

    chore: bump electron-log 4.4.8 => 5.3.3 (#2604)

--- packages/uhk-web/src/renderer/services/electron-log.service.ts
+++ packages/uhk-web/src/renderer/services/electron-log.service.ts
@@ -1,23 +1,7 @@
-import * as path from 'path';
 import { Injectable } from '@angular/core';
-import * as log from 'electron-log';
+import * as log from 'electron-log/renderer';
 import { LogService, logUserConfigHelper, UserConfiguration } from 'uhk-common';
 
-log.transports.file.resolvePath = variables => {
-    return path.join(variables.libraryDefaultDir, 'uhk-agent.log');
-};
-log.transports.file.level = 'silly';
-log.transports.ipc.level = 'silly';
-
-/**
- * This service use the electron-log package to write log in file.
- * The logger usable in main and renderer process.
- * The location of the log files:
- * - on Linux: ~/.config/<app name>/uhk-agent.log
- * - on OS X: ~/Library/Logs/<app name>/uhk-agent.log
- * - on Windows: %USERPROFILE%\AppData\Roaming\<app name>\uhk-agent.log
- * The app name: UHK Agent. The up to date value in the scripts/release.js file.
- */
 @Injectable()
 export class ElectronLogService extends LogService {
 
