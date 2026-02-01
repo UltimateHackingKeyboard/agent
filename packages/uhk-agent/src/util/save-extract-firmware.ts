d5a4494f7678f71eb17d3c387e7ce0fda82b4931
Kiss Róbert <ert78gb@gmail.com>
2026-02-01 19:49:48 +0100

    feat: make the firmware update also accept a CI-generated zip file

--- packages/uhk-agent/src/util/save-extract-firmware.ts
+++ packages/uhk-agent/src/util/save-extract-firmware.ts
@@ -4,6 +4,7 @@ import { dirSync } from 'tmp';
 import decompress from 'decompress';
 import decompressTarbz from 'decompress-tarbz2';
 import decompressTargz from 'decompress-targz';
+import decompressUnzip from 'decompress-unzip';
 import { UploadFileData } from 'uhk-common';
 import { TmpFirmware } from 'uhk-usb';
 
@@ -24,6 +25,28 @@ export async function saveTmpFirmware(fileData: UploadFileData): Promise<TmpFirm
         case '.gz': {
             await decompress(zipFilePath, tmpDirectory.name, { plugins: [decompressTargz()] });
             break;
+        }
+
+        case '.zip': {
+            const unzipOptions: decompress.DecompressOptions = {
+                filter: file => {
+                    const fileName = path.basename(file.path);
+
+                    return fileName.startsWith('uhk-firmware-') && fileName.endsWith('.tar.gz');
+                },
+                plugins: [decompressUnzip()]
+            }
+
+            const unzippedFiles = await decompress(zipFilePath, tmpDirectory.name, unzipOptions);
+            if (unzippedFiles.length === 0) {
+                throw new Error('Zip file does not contain uhk-firmware-*.tar.gz file.');
+            }
+
+            return saveTmpFirmware({
+                data: unzippedFiles[0].data as any,
+                filename: path.basename(unzippedFiles[0].path),
+                saveInHistory: fileData.saveInHistory,
+            })
         }
 
         default:
