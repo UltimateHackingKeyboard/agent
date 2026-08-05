930d9891904b0d84971f3e80af264debf3a63170
Kiss Róbert <ert78gb@gmail.com>
2026-08-05 20:30:42 +0200

    style: allow @angular-eslint/template/alt-text-error rule

--- packages/uhk-web/src/app/store/reducers/calculate-layer-options-of-keymap.ts
+++ packages/uhk-web/src/app/store/reducers/calculate-layer-options-of-keymap.ts
@@ -6,6 +6,11 @@ import { initLayerOptions } from './layer-options';
 export function calculateLayerOptionsOfKeymap(keymap: Keymap): Map<number, LayerOption> {
     const layerOptions = initLayerOptions();
 
+    // it can happen when the user changes the URL to invalid keymap abbreviation
+    if (!keymap) {
+        return layerOptions;
+    }
+
     for (const layer of keymap.layers) {
         layerOptions.get(layer.id).selected = true;
     }
